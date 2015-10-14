b1c196a48b5c67409fd0e938ef1196d99fc2539c
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2015-10-14 19:02:03 -0400

    Initial Commit

---
+++ calculator.js
@@ -0,0 +1,20 @@
+var calc = {
+  add: function (num1,num2) {
+    console.log(num1+num2);
+  },
+  subtract: function (num1,num2) {
+    console.log(num1-num2);
+  },
+  multiply: function (num1,num2) {
+    console.log(num1*num2);
+  },
+  divide: function (num1,num2) {
+    console.log(num1/num2);
+  },
+  exp: function (num1,num2) {
+    console.log(Math.pow(num1,num2));
+  },
+  circleArea: function (r) {
+    console.log(Math.PI*(r*r));
+  }
+}
