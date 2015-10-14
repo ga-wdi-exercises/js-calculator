c5863172de19936c71c39fa5df4956a66bbb0124
Josh Cohen <jacohen10@EMAIL.COM>
2015-10-14 19:09:10 -0400

    finished hw w/o bonus

---
+++ calculator.js
@@ -0,0 +1,25 @@
+var calculator = {
+  add: function(x,y) {
+    return x + y;
+  },
+
+  subtract: function(x,y) {
+    return y - x;
+  },
+
+  multiply: function(x,y) {
+    return x * y;
+  },
+
+  divide: function(x,y) {
+    return (x/y);
+  },
+
+  exponent: function(x,y) {
+    return Math.pow(x,y);
+  },
+
+  square: function(x) {
+    return (x*x);
+  }
+};
