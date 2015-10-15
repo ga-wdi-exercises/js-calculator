6d8ff65c5ecf320471e325c599dbee717bbe70db
Peter Zhang <pzhang87@gmail.com>
2015-10-15 14:14:48 -0400

    add calculator methods

---
+++ calculator.js
@@ -0,0 +1,26 @@
+calculator = {
+  add: function(num1, num2) {
+    result = num1 + num2;
+    return result;
+  },
+  subtract: function(num1, num2) {
+    result = num1 - num2;
+    return result;
+  },
+  multiplication: function(num1, num2) {
+    result = num1 * num2;
+    return result;
+  },
+  division: function(num1, num2) {
+    result = num1 / num2;
+    return result;
+  },
+  power: function(num1, num2) {
+    result = Math.pow(x,y);
+    return result;
+  },
+  sqrt: function(num1) {
+    result = Math.sqrt(num1);
+    return result;
+  },
+}
