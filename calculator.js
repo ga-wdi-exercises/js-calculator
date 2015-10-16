8bfe1900a59efa4a84f46d29fd465d38fab04636
Chauncey Manson <Mansonchauncey@gmail.com>
2015-10-16 16:43:42 -0400

    Calculator

---
+++ calculator.js
@@ -0,0 +1,21 @@
+var calculator = {}
+
+calculator.add = function(num1,num2){
+return num1 + num2;
+}
+
+calculator.subtract = function(num1,num2){
+return num1 - num2;
+}
+
+calculator.mutiply = function(num1,num2){
+return num1 * num2;
+}
+
+calculator.divide = function(num1,num2){
+return num1 / num2;
+}
+
+calculator.exponents = function(num1,num2){
+return Math.pow(num1,num2);
+}
