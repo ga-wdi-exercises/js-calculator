609ccd114dd3c1737619ad6e54a70e78b5a9271c
Rebecca G. Estes <estes.rebecca@gmail.com>
2015-10-15 23:59:37 -0400

    bonus 1 is complete

--- calculator.js
+++ calculator.js
@@ -2,29 +2,40 @@
 // in sequence would result in a value of 10.
 // calculator.add(1,2) is 3, calculator.add(2,2) is 4, calculator.mutliply(2,2) is 4 --> does not equal 10?????
 
+var value;
+
 var calculator = {
   add: function(num1, num2) {
     var sum = num1 + num2;
+    value += sum;
     return sum;
   },
   subtract: function(num1, num2) {
     var diff = num1 - num2;
+    value += diff;
     return diff;
   },
   multiply: function(num1, num2) {
     var product = num1 * num2;
+    value += product;
     return product;
   },
   divide: function(num1, num2) {
     var quotient = num1 / num2;
+    values += quotient;
     return quotient;
   },
   exponent: function(num1, num2) {
     var expo = Math.pow(num1, num2);
+    value += expo;
     return expo;
   },
   squareroot: function(num1) {
     var root = Math.sqrt(num1);
+    value += root;
     return root;
+  },
+  clear: function() {
+    value = 0;
   }
 };
