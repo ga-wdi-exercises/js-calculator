cf4f2d08eab8590445938a3d19eb9b064d30f6e2
Mohamed Goodwin <mohamed.goodwin@gmail.com>
2015-10-16 13:33:50 -0400

    this is the modified calculator homework solution

---
+++ calculator.js
@@ -0,0 +1,14 @@
+var calculator = {
+    add: function (num1, num2) {
+      return num1 + num2
+    },
+    subtract: function (num1, num2) {
+      return num1 - num2
+    },
+    multipiy: function (num1, num2) {
+      return num1 * num2
+    },
+    divide: function (num1, num2) {
+      return num1 / num2
+    }
+};
