719ae2419042e9903e157e7fdfb5b4eb351bfd00
Thomas Clark <tclark@soteriagiving.com>
2015-10-16 08:40:50 -0400

    add js

---
+++ calculator.js
@@ -0,0 +1,23 @@
+var calc = {
+  add: function (addend1, addend2){
+    return addend + addend2;
+  },
+  subtract: function (multiplicand, multiplier){
+    return minuend - subtrahend;
+  },
+  multiply: function (multiplicand, multiplier){
+    return multiplicand * multiplier;
+  },
+  divide: function (divided, divisor){
+    return dividend / divisor;
+  },
+  exponentiate: function (base, exponent) {
+    return Math.pow(base, exponent);
+  }
+};
+
+console.log(calc.add(4,5));
+console.log(calc.subtract)4,5));
+console.log(calc.multiply(4,5));
+console.log(calc.divide(4,5));
+console.log(calc.exponentiate(4,5));
