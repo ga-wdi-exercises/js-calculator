166f44a6400abdcedc423ae1ad70eac0dfdcac89
ckib16 <ckib16@gmail.com>
2015-10-15 22:09:33 -0400

    Basic calculator done

---
+++ calculator.js
@@ -0,0 +1,14 @@
+var calculator = {
+  add: function (x, y) {
+    console.log(x + y);
+  },
+  subtract: function (x, y) {
+    console.log(x - y);
+  },
+  multiply: function (x, y) {
+    console.log(x * y);
+  },
+  divide: function (x, y) {
+    console.log(x / y);
+  }
+};
