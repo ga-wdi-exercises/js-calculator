4808434921dd3339ee17a0b7f21625ddadbab3b8
Brendan Lewis <lewis2ba@dukes.jmu.edu>
2015-10-15 22:40:44 -0400

    simple calculator finished

---
+++ calculator.js
@@ -0,0 +1,20 @@
+var calculator = {
+  add: function(a,b){
+    return a+b;
+  },
+  subtract: function(a,b){
+    return a-b;
+  },
+  multiply: function(a,b){
+    return a*b;
+  },
+  divide: function(a,b){
+    return a/b;
+  },
+  toThePower: function(a,b){
+      return Math.pow(a,b);
+  },
+  remainder: function(a,b){
+    return a%b;
+  }
+}
