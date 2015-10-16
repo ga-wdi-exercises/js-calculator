f712199faed1dce49853127c19f54272c82e7c10
alea q <aleaq@aleas-AppleSmoothie.local>
2015-10-15 21:54:47 -0400

    basic math functions done

--- calculator.js
+++ calculator.js
@@ -1,6 +1,24 @@
 var calculate = {
   add: function (x1, x2){
   return x1 + x2;
+},
+
+
+  subtract: function (x1, x2){
+  return x1 - x2;
+},
+
+
+  multi: function (x1, x2){
+  return x1 * x2;
+},
+
+ 
+  divide: function (x1, x2){
+  return x1 / x2;
 }
 };
-console.log(calculate.add(4,5));
+console.log(calculate.add(1,2));
+console.log(calculate.subtract(1,2));
+console.log(calculate.multi(1,2));
+console.log(calculate.divide(1,2));
