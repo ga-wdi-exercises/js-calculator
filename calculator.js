1e079abd8f5366a815cca898eef94f2c6741e6c4
Peter Zhang <pzhang87@gmail.com>
2015-10-15 20:44:28 -0400

    include memory and logic for methods to work with memory

--- calculator.js
+++ calculator.js
@@ -1,26 +1,31 @@
-calculator = {
+var result = 0;
+calc = {
   add: function(num1, num2) {
-    result = num1 + num2;
+    result = num1 + (num2 || result);
     return result;
   },
   subtract: function(num1, num2) {
-    result = num1 - num2;
+    result = num1 - (num2 || result);
     return result;
   },
   multiplication: function(num1, num2) {
-    result = num1 * num2;
+    result = num1 * (num2 || result);
     return result;
   },
   division: function(num1, num2) {
-    result = num1 / num2;
+    result = (num1 / num2) || (result / num1);
     return result;
   },
   power: function(num1, num2) {
-    result = Math.pow(x,y);
+    result = (num2 ? Math.pow(num1,num2) : Math.pow(result, num1) );
     return result;
   },
   sqrt: function(num1) {
-    result = Math.sqrt(num1);
+    result = Math.sqrt(num1 || result);
+    return result;
+  },
+  clear: function() {
+    result = 0;
     return result;
   },
 }
