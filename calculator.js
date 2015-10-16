b7fa50e5f9c23d08336d543a32d8380a7153e323
James Moon <mooniker@gmail.com>
2015-10-16 09:59:07 -0400

    Add semicolon and make other nonsubstantive stylistic corrections.

--- calculator.js
+++ calculator.js
@@ -1,11 +1,11 @@
 
 
 var calculator = {
-  add: function ( num1, num2 ) { return num1 + num2 },
+  add: function ( num1, num2 ) { return num1 + num2; },
   subtract: function ( num1, num2 ) { return num1 - num2; },
   multiply: function ( num1, num2 ) { return num1 * num2; },
   divide: function ( num1, num2 ) { return num1 / num2; },
-  pow: function ( num1, num2 ) { return Math.pow(num1, num2); },
+  pow: function ( num1, num2 ) { return Math.pow( num1, num2 ); },
   floor_divide: function ( num1, num2 ) { return Math.floor( num1 / num2 ); },
   mod: function ( num1, num2 ) { return num1 % num2; },
   help: function () { console.log("Functions: add, subtract, multiply, divide, floor_divide, mod, pow."); },
