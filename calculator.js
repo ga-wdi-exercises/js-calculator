5751a3b205e3c8c9cabf0125a2650537bf3e52ce
Dennis Truong <dtruong02@gmail.com>
2015-10-16 08:31:40 -0400

    JS Calculator Homework Final

---
+++ calculator.js
@@ -0,0 +1,23 @@
+var calcTools = {
+  mult: function (firstNum, secondNum) {
+    console.log("Your equation is equal to:")
+    return firstNum * secondNum;
+  },
+  add: function (firstNum, secondNum) {
+    console.log("Your equation is equal to:")
+    return firstNum + secondNum;
+  },
+  subt: function (firstNum, secondNum) {
+    console.log("Your equation is equal to:")
+    return firstNum - secondNum;
+  },
+  divide: function (firstNum, secondNum) {
+    console.log("Your equation is equal to:")
+    return firstNum / secondNum;
+  },
+  expo: function (firstNum, secondNum) {
+    console.log("Your equation is equal to:")
+    return Math.pow (firstNum, secondNum);
+  }
+};
+
