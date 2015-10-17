e6b62407645836f1c6dbbf96b5ec7accc7d0d7f0
Rachel Porter <rachelandrewsporter@gmail.com>
2015-10-16 20:18:20 -0400

    add forgotten last operation

--- calculator.js
+++ calculator.js
@@ -14,7 +14,10 @@ var calculator = {
   powerX: function(num1,num2) {
     console.log( Math.pow(num1,num2));
   },
+  circumference: function(num1) {
+    console.log( (Math.PI * (num1 * num1));
+  }
 
 };
 
-console.log("To use this calculator: call calculator.add(num1, num2) or .subtract, .multiply, .division, or .powerX");
+console.log("To use this calculator: call calculator.add(num1, num2) or .subtract, .multiply, .division, or .powerX, or find the circumference of a circle by using .circumference(num)");
