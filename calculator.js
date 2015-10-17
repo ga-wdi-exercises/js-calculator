1e3cc16c6de2ef215abd5621fbfa1e7c62711e66
CHRYSTAL BANKS <Chrystal.Banks1@gmail.com>
2015-10-17 11:18:46 -0400

    Chrystal Calculator

---
+++ calculator.js
@@ -0,0 +1,14 @@
+var calculator = {
+  add: function(num1, num2) {
+     return(num1 + num2);
+  },
+  multiply: function(num1, num2) {
+    return(num1 * num2);
+  },
+  divide: function(num1, num2) {
+     return(num1 / num2);
+  },
+  power: function(num1, num2) {
+    return Math.pow(num1, num2)
+  },
+}
