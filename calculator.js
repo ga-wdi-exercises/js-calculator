b88a55b3bf14eaf26cfd8000b9f95f02bade3ef9
Jared Nichols <jnichols300@gmail.com>
2015-10-18 22:30:04 -0400

    Update calculator.js
    Calculator!

---
+++ calculator.js
@@ -0,0 +1,17 @@
+var calc = {
+	  subtract: function (num1, num2) {
++      return num1 - num2
+}
+
+	  add: function (num1, num2) {
+	  	return num1 + num2
+	  }
+
+	  divide: function (num1, num2) {
+	  	return num1 / num2
+	  }
+
+	  multiply: function (num1, num2) {
+	  	return num1 * num2
+	  }
+	}
