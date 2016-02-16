a1e38aae9036dbc3dd30f47c6d4d92bcf52487d1
Christine Movius <christine.movius@gmail.com>
2016-02-16 17:22:30 -0500

    Added exponent and square root methods.

--- calculator.js
+++ calculator.js
@@ -10,6 +10,12 @@ var calculator = {
   },
   divide: function(num1, num2) {
     return num1/num2;
+  },
+  exponent: function(num1, num2) {
+    return Math.pow(num1, num2);
+  },
+  square_root: function(num1) {
+    return Math.sqrt(num1);
   }
 }
 //calculator.add(3,4);
