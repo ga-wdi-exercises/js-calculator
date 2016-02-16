d3caffd752ca4f318aed25458ca1a6736782cdd4
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-02-16 16:57:25 -0500

    initial commit, created calculator

---
+++ calculator.js
@@ -0,0 +1,16 @@
+var calc = {
+    add: function(num1, num2) {
+        return num1 + num2},
+    subtract: function(num1, num2) {
+        return num1 - num2},
+    multiply: function(num1, num2) {
+        return num1 * num2},
+    division: function(num1, num2) {
+        return num1/num2},
+    exponent: function(num1, num2) {
+        return Math.pow(num1, num2)},
+    squareRoot: function(num1) {
+        return Math.sqrt(num1)}
+}
+
+calc.squareRoot(144)
