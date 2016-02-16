00fd229a518018353ed2526c6992fca2f742d92f
Clarissa Stark <clarissa.stark@gmail.com>
2016-02-16 17:18:19 -0500

    basic calculator working

---
+++ calculator.js
@@ -0,0 +1,24 @@
+var calculator = {
+  add: function(num1,num2) {
+    return num1 + num2;
+  },
+  sub: function(num1,num2) {
+    return num1 - num2;
+  },
+  multiply: function(num1,num2) {
+    return num1 * num2;
+  },
+  divide: function(num1,num2) {
+    return num1 / num2;
+  },
+  exponent: function(num1,num2) {
+    return pow(num1, num2);
+  },
+  remainder: function(num1,num2) {
+    return num1 % num2;
+  }
+  remainder: function(num1,num2) {
+    var remAnswer = function()
+  }
+}
+calculator.add(1,3);
