826ce4a53bcd42e87566dc9251ab6adb77765dce
Caitlin Daitch <caitlin.daitch@gmail.com>
2016-02-16 18:44:06 -0500

    Created working calculator and allowed value to be stored and cleared

---
+++ calculator.js
@@ -0,0 +1,25 @@
+var value;
+
+var calculator = {
+  add: function(a, b) {
+    return value = a + b;
+  },
+  subtract: function(a, b) {
+    return value = a - b;
+  },
+  multiply: function(a, b) {
+    return value = a * b;
+  },
+  divide: function(a, b) {
+    return value = a / b;
+  },
+  exponent: function(a, b) {
+    return value = Math.pow(a, b);
+  },
+  modulo: function(a, b) {
+    return value = a % b;
+  },
+  clear: function() {
+    return value = 0;
+  }
+};
