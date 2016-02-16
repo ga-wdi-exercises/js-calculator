9b4cdd419b029774b0da1286b3ae22ece16946e3
Christine Movius <christine.movius@gmail.com>
2016-02-16 17:14:37 -0500

    multiply and divide methods.

--- calculator.js
+++ calculator.js
@@ -1,10 +1,15 @@
 var calculator = {
-  add: function(num1, num2){
+  add: function(num1, num2) {
     return num1 + num2;
   },
   subtract: function(num1, num2) {
     return num1 - num2;
+  },
+  multiply: function(num1, num2) {
+    return num1 * num2;
+  },
+  divide: function(num1, num2) {
+    return num1/num2;
   }
-
 }
 //calculator.add(3,4);
