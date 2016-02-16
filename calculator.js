c84ac0586c6e2f5a69a216fefe79d9bbfdaa978a
Anissa Jones <anissajones10@gmail.com>
2016-02-16 18:40:25 -0500

    basic frame of first four functions.

---
+++ calculator.js
@@ -0,0 +1,22 @@
+var jsCal = {
+  add: function(num1, num2){
+    return(num1 + num2);
+  },
+
+  sub: function(num1, num2){
+    return(num1 - num2);
+  },
+
+  multiply: function(num1, num2){
+    return(num1 * num2);
+  },
+
+  divid: function(num1, num2){
+    return(num1 / num2);
+  },
+
+  expo: function(){
+
+  },
+
+}
