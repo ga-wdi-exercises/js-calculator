f77c6f463bbdbe7f5b8b3e4466cebac057f1ebf6
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-02-16 22:21:00 -0500

    built basic calculator code

---
+++ calculator.js
@@ -0,0 +1,21 @@
+var calculate = {
+  add: function(n1,n2){
+    return n1 + n2;
+    },
+  subtract:function(n1,n2){
+    return n1 - n2;
+    },
+  multiply:function(n1,n2){
+    return n1 * n2;
+    },
+  divide:function(n1,n2){
+    return n1 / n2;
+    },
+  /*Let it be known that I had to google what exponents even were again...much less the math function -- neat though!;) */
+  exp:function(n1){
+    return Math.exp(n1);
+    },
+  squareroot:function(n1){
+         return Math.sqrt(n1);
+  },
+};
