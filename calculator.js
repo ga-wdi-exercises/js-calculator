532d661c3a1cbffe9e255a5a18058805421571f8
Cam <camlatimer@gmail.com>
2016-02-16 22:35:08 -0500

    initial commit

---
+++ calculator.js
@@ -0,0 +1,57 @@
+var calc = {
+  add: function (a, b) {
+	  return a + b;
+	},
+  sub: function (a, b) {
+	  return a - b;
+	},
+  multi: function (a, b) {
+	  return a * b;
+	},
+  divide: function (a, b) {
+    return a / b;
+  },
+  powers: function (a, b) {
+	  return Math.pow(a, b);
+	},
+  //operation of choice
+  squarert: function (a) {
+    return Math.sqrt(a);
+  }
+};
+
+//bonus 1 + 2
+var calcBonus = {
+  value: 0,
+  add: function (a) {
+	  return this.value +=  a;
+	},
+  sub: function (a) {
+	  return this.value -= a;
+	},
+  multi: function (a, b) {
+	  return this.value *= a;
+	},
+  divide: function (a) {
+    return this.value /= a;
+  },
+  powers: function (a, b) {
+    this.clear();
+	  return this.value = Math.pow(a, b);
+	},
+  squarert: function (a) {
+    return Math.sqrt(this.value);
+  },
+  master: function (str) {
+    str = str.toString();
+    return this.value += eval(str);
+  },
+  clear: function () {
+	  calc.value = 0;
+	}
+};
+
+/*
+the bonus section sort of gives the calculator 'memory', but maybe not the way
+it's described in the bonus section.  I wasn't sure how to make that example
+sequence evaluate to 10.  
