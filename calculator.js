84400ff730bd65c31d53569cdc574529e3130852
Fatai Agiri <yinka.fatai@gmail.com>
2016-02-17 17:50:19 -0500

    memory added

--- calculator.js
+++ calculator.js
@@ -1,23 +1,30 @@
 var calculator = {
+  value: null;
   add: function(num1, num2) {
-    return num1 + num2;
+    this.value = num1 + num2;
+    return this.value;
   },
   subtract: function(num1, num2) {
-    return num1 - num2;
+    this.value = num1 - num2;
+    return this.value;
   },
   multiply: function(num1, num2) {
-    return num1 * num2;
+    this.value = num1 * num2;
+    return this.value;
   },
   divide: function(num1, num2) {
-    if((num1 && num2)!===0) {
-      return num1 / num2;
-    },
+    if((num1 && num2)!== 0) {
+      this.value = num1 / num2;
+      return this.value;
+    }
     else return NaN;
   },
   pow: function(num1, num2) {
-    return Math.pow(num1, num2);
+    this.value = Math.pow(num1, num2);
+    return this.value;
   },
   sqrt: function(num1) {
-    return Math.sqrt(num1);
+    this.value = Math.sqrt(num1);
+    return this.value;
   }
 }
