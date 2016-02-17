21822375f9c9342dd945cc98f34b957a61cc1f1c
Fatai Agiri <yinka.fatai@gmail.com>
2016-02-17 17:36:21 -0500

    basic calculator completed

--- calculator.js
+++ calculator.js
@@ -1,1 +1,23 @@
-var calculator
+var calculator = {
+  add: function(num1, num2) {
+    return num1 + num2;
+  },
+  subtract: function(num1, num2) {
+    return num1 - num2;
+  },
+  multiply: function(num1, num2) {
+    return num1 * num2;
+  },
+  divide: function(num1, num2) {
+    if((num1 && num2)!===0) {
+      return num1 / num2;
+    },
+    else return NaN;
+  },
+  pow: function(num1, num2) {
+    return Math.pow(num1, num2);
+  },
+  sqrt: function(num1) {
+    return Math.sqrt(num1);
+  }
+}
