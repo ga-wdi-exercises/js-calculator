2d1c426bf43c5e3d57f0d6262597c69c688f4d4b
Anissa Jones <anissajones10@gmail.com>
2016-02-16 21:33:06 -0500

    Trying to figure out operator function.

--- calculator.js
+++ calculator.js
@@ -15,8 +15,21 @@ var jsCal = {
     return(num1 / num2);
   },
 
-  expo: function(){
-
+  expo: function(num1, num2){
+    var finalNum = 1;
+    for(i = 0; i<num2; i++){
+        finalNum *= num1;
+        console.log("i is " + i + " " + "so far.");
+        console.log("num1 equals," + num1);
+    }
+    return(finalNum);
   },
+  /* I thought this would work,
+  I looked it up and even saw this exact answer in stackoverflow
+  but for some reason, num1 stays the same.
+  */
 
+  otherOperate: function(){
+    return(console.log("Almost there."))
+  }
 }
