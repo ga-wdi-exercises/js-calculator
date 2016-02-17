4a2b8605514ad7c7a22ec5a666013ff265808b49
Anissa Jones <anissajones10@gmail.com>
2016-02-17 14:32:31 -0500

    expo function works, need to test averageNum.

--- calculator.js
+++ calculator.js
@@ -19,15 +19,9 @@ var jsCal = {
     var finalNum = 1;
     for(i = 0; i<num2; i++){
         finalNum = finalNum * num1;
-        console.log("i is " + i + " " + "so far.");
-        console.log("num1 equals," + num1);
     }
     return(finalNum);
   },
-  /* I thought this would work,
-  I looked it up and even saw this exact answer in stackoverflow
-  (in C) but for some reason, num1 stays the same.
-  */
 
   averageNum: function(x){
     //must pass an array of numbers
