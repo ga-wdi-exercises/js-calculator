d9b665d02563b314f56067245d60cc178ab6c22a
Nyron Curtis Waite <Nyroncurtiswaite@gmail.com>
2016-02-16 21:52:28 -0500

    changes

---
+++ calculator.js
@@ -0,0 +1,24 @@
+var calculator = {
+
+add: function(a , b){
+  console.log(a + b);
+  return a + b;
+} ,
+subtract: function(a , b){
+  console.log(a - b);
+} ,
+multiply: function(a , b){
+  console.log(a * b);
+} ,
+divide: function(a , b){
+  console.log(a / b);
+}
+
+
+
+
+}
+calculator.add();
+calculator.subtract();
+calculator.multiply();
+calculator.divide();
