083ef8fea6db9da6f7fe8a8eb3dc8cb45d959f79
Andrew Smith-Mui <arsm800@gmail.com>
2016-02-17 21:34:15 -0500

    js calc no bonus

---
+++ calculator.js
@@ -0,0 +1,20 @@
+var calculator = {
+  add: function (x, y) {
+    console.log(x + y);
+  },
+  subtract: function (a, b) {
+    console.log(a - b);
+  },
+  multiply: function (c, d) {
+    console.log(c * d);
+  },
+  divide: function (e, f) {
+    console.log(e / f);
+  },
+  exponent: function (g, h) {
+    console.log(Math.pow(g, h));
+  },
+  remainder: function (i, j) {
+    console.log(i % j);
+  }
+}
