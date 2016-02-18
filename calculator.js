f86804962636919a58b45b08e778c59218a5f3d7
Nyron Curtis Waite <Nyroncurtiswaite@gmail.com>
2016-02-17 19:17:09 -0500

    new

--- calculator.js
+++ calculator.js
@@ -12,12 +12,16 @@ multiply: function(a , b){
 } ,
 divide: function(a , b){
   console.log(a / b);
+} ,
+exp: function(a, b){
+	return Math.pow(a, b)
+} ,
+per: function(a ,b){
+	return((a / b)*100)
 }
 
 
-
-
-}
+};
 calculator.add();
 calculator.subtract();
 calculator.multiply();
