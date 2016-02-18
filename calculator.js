d9b5ae372ebb835bda9d7e30fc8ba504b1c39adc
Woo Yun <hh747hh@gmail.com>
2016-02-17 22:04:17 -0500

    homework done

---
+++ calculator.js
@@ -0,0 +1,18 @@
+var calculator = {
+  add : function (num1,num2){
+      num1 + num2
+      console.log(num1+num2);
+    },
+     sub :  function (num1, num2){
+        num1 - num2
+        console.log(num1 - num2);
+      },
+       multiply :  function (num1,num2) {
+        num1 * num2
+        console.log(num1 * num2);
+    },
+       division :function (num1, num2) {
+    num1 / num2
+        console.log(num1/num2);
+    }
+}
