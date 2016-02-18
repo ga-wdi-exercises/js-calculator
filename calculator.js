576fd5a9c38f24468eab7fd048d38b0fbcb0287a
Lyndsey Newsome <lyndseysnewsome@gmail.com>
2016-02-17 22:27:17 -0500

    C.1

---
+++ calculator.js
@@ -0,0 +1,20 @@
+var calculator ={
+add:function (x,y) {
+  return x+y;
+}
+}
+  var calculator ={
+  subtract:function (x,y) {
+    return x-y;
+}
+}
+    var calculator ={
+    multiply:function (x,y) {
+      return x*y;
+}
+}
+    var calculator ={
+    divide:function (x,y) {
+      return x/ y;
+}
+}
