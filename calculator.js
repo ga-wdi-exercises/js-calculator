cb7d6195b029b2491056481cc128ff5370755db2
Hilary Masland <hilarymasland@gmail.com>
2016-02-17 19:04:28 -0500

    Intitial commit Calculator Homework

---
+++ calculator.js
@@ -0,0 +1,22 @@
+
+function Addition(num1, num2){
+    return (num1 + num2);
+  }
+
+  function Subtraction(num1, num2){
+    return (num1 - num2);
+  }
+
+  function multiply(num1, num2){
+      return (num1 * num2);
+    }
+
+  function divide(num1, num2){
+      return (num1 / num2);
+    }
+  function Addmultiply(num1, num2){
+      return (num1 + num2 * (num1, num2) )
+    }
+  function exponent(num1, num2){
+    return (math.pow(num1, num2))
+  }
