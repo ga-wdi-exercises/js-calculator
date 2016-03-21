c91caefe4c962a7224b76bf3e5a7ee3cb5fb24fd
Leland Jordon <lelandjordon@mac.com>
2016-03-21 16:47:54 -0400

    Basic functions working in a js console.

---
+++ calculator.js
@@ -0,0 +1,19 @@
+
+//calculator(){
+
+function add(num1, num2){
+  return num1 + num2;
+}
+function subtract(num1, num2){
+  return num1-num2;
+};
+
+function multiply(num1, num2){
+  return num1*num2;
+}
+function divide(num1, num2){
+  return num1/num2;
+}
+function powerOf(base, exp){
+  return Math.pow(base, exp)
+}
