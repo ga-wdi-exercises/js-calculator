eb5290d4164ec70ee39cb85dc048578357dfb671
Spicy Legato <jpqreichard@gmail.com>
2016-03-21 13:44:59 -0400

    initial commit

---
+++ calculator.js
@@ -0,0 +1,35 @@
+var calculator = {
+  value: 0,
+  clear: function() {
+    value=0;
+  },
+
+  add: function (x,y) {
+    value=x+y;
+    return value;
+  },
+
+  subtract: function (x,y) {
+    value=x-y;
+    return value;
+  },
+
+  multiply: function (x,y) {
+    value=x*y;
+    return value;
+  },
+
+  divide: function (x,y) {
+    value=x/y;
+    return value;
+  },
+
+  exp: function (base,power) {
+    var result=1;
+    for (var i = 0; i < pow; i++){
+      result = result * base;
+    }
+    return result;;
+  }
+
+}
