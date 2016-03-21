a0ec2eace986b416208e51bb6b10c3552eb8495e
Matt <mvaden08@gmail.com>
2016-03-21 14:12:28 -0400

    Completed js.calculator. No bonus (for now). GROWTH MINDSETgit statusgit status

---
+++ calculator.js
@@ -0,0 +1,31 @@
+//Add
+function [
+      function addition(num1, num2){
+            return num1 + num2;
+            console.log(addition(num1, num2));
+      }
+      function subtraction (num1, num2){
+            return num1 - num2;
+            console.log(subtraction(num1, num2));
+      }
+      function multiply (num1, num2){
+            return num1 * num2;
+            console.log(multiply(num1, num2));
+      }
+      function multiply (num1, num2){
+            return num1 * num2;
+            console.log(multiply(num1, num2));
+      }
+      function divide (num1, num2){
+            return num1 / num2;
+            console.log(divide(num1, num2));
+      }
+      function exponents Math.pow(){
+            return Math.pow
+            console.log(math.pow)
+      }
+]
+
+val value = []{
+
+}
