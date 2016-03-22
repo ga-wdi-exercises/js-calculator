195a37d81b9aef4e2520c4a977063fbd7d2633e9
Leland Jordon <lelandjordon@mac.com>
2016-03-21 23:48:56 -0400

    Experimenting with a memory calculator.

--- calculator.js
+++ calculator.js
@@ -18,3 +18,30 @@ var calculator = {
     return Math.pow(base, exp)
   }
 };
+// _------- ^^^^^ working basic calculator above /\ /\ /\ /\
+//  \/ \/ \/ \/  testing memory calculator here \/\|/ \/ \/ \/ \/
+var calcMemory = {
+  add: function(num3){
+    value = num1 + num2;
+  return value;
+},
+subtract: function(num3){
+    value = num1 - num2;
+  return value;
+},
+
+multiply: function(num3){
+    value = num1*num2;
+   return value;
+},
+divide: function(num3){
+    value = num1/num2;
+   return value;
+},
+powerOf: function(num3){
+  value = Math.pow(num1, num2);
+  return value;
+}
+
+num1 = value;
+}
