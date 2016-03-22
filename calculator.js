5020c0e77dd8e7cb14038856fba3daed354161ab
Hewan Abreha <henebys@gmail.com>
2016-03-22 07:00:38 -0400

    Homework on Calculator

---
+++ calculator.js
@@ -0,0 +1,25 @@
+function
+  function addition (num1, num2){
+    return num1 + num2;
+    console.log(addtion(num1, num2));
+  }
+
+  function addition (num1, num2){
+    return num1 - num2;
+    console.log(addtion(num1, num2));
+  }
+  function addition (num1, num2){
+    return num1 * num2;
+    console.log(addtion(num1, num2));
+  }
+  function addition (num1, num2){
+    return num1 / num2;
+    console.log(addtion(num1, num2));
+  }
+  function Exponent Math.pow(){
+    return Math.pow
+    console.log(math.pow);
+  }
+
+  var value =[] {
+  }
