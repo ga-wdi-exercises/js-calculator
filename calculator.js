9abeb476fbc20e60d63a9b82229ad5a7f4eec679
Leland Jordon <lelandjordon@mac.com>
2016-03-21 23:57:27 -0400

    Memory function works now.

--- calculator.js
+++ calculator.js
@@ -2,46 +2,55 @@
 //calculator(){
 var calculator = {
   add: function(num1, num2){
-    return num1 + num2;
+    value = num1 + num2;
+  return value;
   },
   subtract: function(num1, num2){
-    return num1-num2;
+    value = num1 - num2;
+  return value;
   },
 
   multiply: function(num1, num2){
-    return num1*num2;
+    value = num1 * num2;
+   return value;
   },
   divide: function(num1, num2){
-    return num1/num2;
+    value = num1 / num2;
+   return value;
   },
-  powerOf: function(base, exp){
-    return Math.pow(base, exp)
+  powerOf: function(num1, num2){
+    value = Math.pow(num1, num2);
+    return value;
   }
 };
 // _------- ^^^^^ working basic calculator above /\ /\ /\ /\
-//  \/ \/ \/ \/  testing memory calculator here \/\|/ \/ \/ \/ \/
+//  \/ \/ \/ \/  Working memory calculator here \/\|/ \/ \/ \/ \/
+
+// Instructions: MUST USE CALCULATOR above first! This one only uses
+// the previous result.
+
 var calcMemory = {
   add: function(num3){
-    value = num1 + num2;
+    value = value + num3;
   return value;
 },
 subtract: function(num3){
-    value = num1 - num2;
+    value = value - num3;
   return value;
 },
 
 multiply: function(num3){
-    value = num1*num2;
+    value = value * num3;
    return value;
 },
 divide: function(num3){
-    value = num1/num2;
+    value = value / num3;
    return value;
 },
 powerOf: function(num3){
-  value = Math.pow(num1, num2);
+    value = Math.pow(value, num3);
   return value;
 }
 
-num1 = value;
+//num1 = value;
 }
