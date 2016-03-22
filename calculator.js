4895dd166917f91ac3a46f6809a6957264b646ff
Hewan Abreha <henebys@gmail.com>
2016-03-22 09:28:32 -0400

    undated homework on calc

--- calculator.js
+++ calculator.js
@@ -1,25 +1,33 @@
-function
-  function addition (num1, num2){
+var calculator = {
+
+
+
+
+  addition: function add(num1, num2){
     return num1 + num2;
-    console.log(addtion(num1, num2));
-  }
-
-  function addition (num1, num2){
+    // console.log(addtion(num1, num2));
+  },
+subt: function sub(num1, num2) {
     return num1 - num2;
-    console.log(addtion(num1, num2));
-  }
-  function addition (num1, num2){
+    // console.log(addtion(num1, num2));
+  },
+  multiplication: function mult(num1, num2){
     return num1 * num2;
-    console.log(addtion(num1, num2));
-  }
-  function addition (num1, num2){
+    // console.log(addtion(num1, num2));
+  },
+  division: function divide(num1, num2){
     return num1 / num2;
-    console.log(addtion(num1, num2));
+    // console.log(addtion(num1, num2));
+  },
+  exponent: function exp(num1, num2){
+    return Math.pow(num1, num2);
+    // console.log(math.pow);
   }
-  function Exponent Math.pow(){
-    return Math.pow
-    console.log(math.pow);
-  }
-
-  var value =[] {
-  }
+};
+// exponent: function(base, exponent){
+// retun Math.pow(base, exponent)
+//}
+//num1 is a property of key value object.
+  // var value =[] {
+  // }
+//java script exponent
