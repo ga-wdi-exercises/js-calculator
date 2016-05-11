b46c4a67dc10f90ab40fed8c642bb5413a1e8842
Matt <mvaden08@gmail.com>
2016-05-11 19:22:03 -0400

    Initial commit.

---
+++ my_calculator.js
@@ -0,0 +1,20 @@
+// This is my "starter code". I generated it from scatch.
+
+// function calculator (
+    function addition( num1, num2 ){
+      console.log( num1 * num2 );
+      return( num1 * num2);
+  }
+
+    // function subtraction( num1, num2 ){
+    //
+    // }
+    //
+    // function multiply( num1, num2 ){
+    //
+    // }
+    //
+    // function divide( num1, num2 ){
+    //
+    // }
+// );
