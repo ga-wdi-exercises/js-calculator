08274a78fb9518f9cd75b1ce3f52b9a5e7950d16
Matt <mvaden08@gmail.com>
2016-05-11 20:17:21 -0400

    console.log is now reachable.

--- my_calculator.js
+++ my_calculator.js
@@ -1,10 +1,12 @@
 // This is my "starter code". I generated it from scatch.
 
-// function calculator (
-    function addition( num1, num2 ){
-      console.log( num1 * num2 );
-      return( num1 * num2);
+function math (
+    function addition( num1, num2 ) {
+      return(addition( num1 + num2 ));
+      // console.log( num1 * num2 );
   }
+
+
 
     // function subtraction( num1, num2 ){
     //
@@ -17,4 +19,4 @@
     // function divide( num1, num2 ){
     //
     // }
-// );
+)
