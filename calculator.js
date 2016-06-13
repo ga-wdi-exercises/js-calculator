b534f473dbc60438b9d91321c207b331f14cecd9
Adina Pollack <adinap16@gmail.com>
2016-06-13 17:28:14 -0400

    attempted bonus

---
+++ calculator.js
@@ -0,0 +1,27 @@
+var calculator = {
+  value: 0,
+  add: function(value, y) {
+    value = value + y;
+    return value;
+  },
+  subtract: function(value, y) {
+    value =  value - y
+    return value;
+  },
+  multiply: function(value, y) {
+    value = value * y
+    return value;
+  },
+  divide: function(value, y) {
+    value = value / y
+    return value
+  },
+  exponents: function(value, y) {
+    value = Math.pow();
+    return value
+  },
+  square: function(value){
+    value = value * value
+    return value
+  }
+}
