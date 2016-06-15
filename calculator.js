140e031c17e2f9ebd553b95840509835b0d57f10
Andrew Young <prince.of.utah@gmail.com>
2016-06-14 21:26:22 -0400

    Initial Commit

---
+++ calculator.js
@@ -0,0 +1,12 @@
+
+
+//Javascript Calculat
+
+//Your task is to create a Javascript object that represents a calculator. It should have methods that provide it with the following functionality...
+
+//Addition
+//Subtraction
+//Multiplication
+//Division
+//Exponents
+//You should be able to run these methods like so from your browser's Javascript console...
