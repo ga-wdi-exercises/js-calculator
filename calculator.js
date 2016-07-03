28b6b693aa69b4f3d298944af3773d43e06205eb
Tyler Rosenberg <tyryanr@gmail.com>
2016-07-03 18:36:16 -0400

    completed Javascript Calculator and Bonus 1

---
+++ calculator.js
@@ -0,0 +1,80 @@
+var calculator = {
+  value: 0;
+  add: function (num1, num2) {
+    return num1 + num2
+  },
+  subtract: function (num1, num2) {
+    return num1 - num2;
+  },
+  multiply: function (num1, num2) {
+    return num1 * num2;
+  },
+  divide: function (num1, num2) {
+    return num1 * num2;
+  },
+  exponents: function (num1, num2) {
+    return Math.pow(num1, num2);
+  },
+  random: function () {
+    return (Math.random() * 10) + 1;
+  },
+
+}
+
+
+
+### BONUS 1)
+
+var calculator = {
+  value: 0,
+  add: function (num) {
+    calculator.value = calculator.value + num;
+    return calculator.value;
+  },
+  subtract: function (num) {
+    calculator.value = calculator.value - num;
+    return calculator.value;
+  },
+  multiply: function (num) {
+    calculator.value = calculator.value * num;
+    return calculator.value;
+  },
+  divide: function (num) {
+    calculator.value = calculator.value / num;
+    return calculator.value;
+  },
+  clear: function () {
+    calculator.value = 0;
+    return calculator.value;
+  }
+}
+
+
+BONUS 2)
+
+var calculator = {
+  value: 0,
+  master: function (string) {
+    calculator.value = calculator.value + parseInt(string);
+  }
+  add: function (num) {
+    calculator.value = calculator.value + num;
+    return calculator.value;
+  },
+  subtract: function (num) {
+    calculator.value = calculator.value - num;
+    return calculator.value;
+  },
+  multiply: function (num) {
+    calculator.value = calculator.value * num;
+    return calculator.value;
+  },
+  divide: function (num) {
+    calculator.value = calculator.value / num;
+    return calculator.value;
+  },
+  clear: function () {
+    calculator.value = 0;
+    return calculator.value;
+  }
+}
