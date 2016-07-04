c88a503c52002ca7bbb8309c43bc936d18459679
Christopher Phillips <cphillips918@gmail.com>
2016-07-04 17:31:14 -0400

    working on bonuses tonight after quiz and when I work on my adventure hw

---
+++ calculator.js
@@ -0,0 +1,22 @@
+calculator = {}
+var calculator = {
+    add: function(number1, number2){
+        return number1+number2;
+    },
+    subtract: function(number1, number2){
+        return number1-number2;
+    },
+    multiply: function(number1, number2){
+        return number1*number2;
+    },
+    divide:function(number1, number2){
+        return number1/number2;
+    },
+    power: function(number1, number2){
+        return Math.pow(number1, number2)
+
+    },
+    letsGetCreative: function(){
+
+    }
+};
