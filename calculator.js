d23c63bed1d1b479d0aef1f488ccb3f1665f622f
Chase <chasefreeman13@gmail.com>
2016-07-04 17:37:57 -0400

    Exponents work

--- calculator.js
+++ calculator.js
@@ -2,5 +2,23 @@ var calculate = {
   //addition
     add: function(num1,num2) {
       return num1 + num2
-    }
+    },
+    //subtraction
+    subtract: function(num1,num2) {
+      return num1 - num2
+    },
+    //multiplication
+    multiply: function(num1,num2) {
+      return num1 * num2
+    },
+    //division
+    divide: function(num1,num2) {
+      return num1 / num2
+    },
+    //exponenets
+    powerOf: function(num1,num2) {
+      return Math.pow(num1, num2);
+    },
+
+
   };
