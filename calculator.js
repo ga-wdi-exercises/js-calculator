dc17621650f570d44b45f63e28a69c8ee6eba3f7
Chase <chasefreeman13@gmail.com>
2016-07-04 18:09:22 -0400

    changed value store code--works until cleared

--- calculator.js
+++ calculator.js
@@ -4,17 +4,28 @@ var calculate = {
   //addition
     add: function(num1,num2) {
       if (num2 === undefined) {
-  //code for value being added to num1
-        return num1 + calculate.value;
+  //code for value being added to num1;
+        calculate.value = num1 + calculate.value;
+        return calculate.value
+
       }
       else {
           return num1 + num2
         }
+
+
     },
   //subtraction
-    subtract: function(num1,num2) {
-      return num1 - num2
-    },
+  subtract: function(num1,num2) {
+    if (num2 === undefined) {
+//code for num1 being subtracted from value (this is important & opposite from Addition!)
+      return calculate.value - num1;
+    }
+    else {
+      //notice num1 is in a different location than above!
+        return num1 - num2
+      }
+  },
     //multiplication
     multiply: function(num1,num2) {
       return num1 * num2
