f5ca707f40abba017275d410f76a5bf8777ae8ae
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2016-07-04 18:40:41 -0400

    added remainder function

--- calculator.js
+++ calculator.js
@@ -19,5 +19,10 @@ divide: function divide(num1, num2) {
 exponent: function exponent(num1, num2) {
   console.log(num1, num2);
   return Math.pow(num1, num2);
+  //can also use ** operator for exponentiation
+},
+remainder: function remainder(num1, num2) {
+  console.log(num1, num2);
+  return num1 % num2;
 },
 }
