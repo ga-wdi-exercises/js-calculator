659b127cb405878e2544d8b16caddf2590896c01
Chase <chasefreeman13@gmail.com>
2016-07-04 18:30:18 -0400

    finished the JS calculator + Bonus 1

--- calculator.js
+++ calculator.js
@@ -1,43 +1,62 @@
 var calculate = {
   //avlue store
-    value: 2,
+    value: 0,
   //addition
     add: function(num1,num2) {
       if (num2 === undefined) {
   //code for value being added to num1;
         calculate.value = num1 + calculate.value;
         return calculate.value
-
       }
       else {
           return num1 + num2
         }
-
-
     },
-  //subtraction
+//subtraction
   subtract: function(num1,num2) {
     if (num2 === undefined) {
-//code for num1 being subtracted from value (this is important & opposite from Addition!)
-      return calculate.value - num1;
+//code for num1 being subtracted from stored value first (opposite of addtion)
+      calculate.value = calculate.value - num1;
+      return calculate.value
     }
     else {
-      //notice num1 is in a different location than above!
+      //this code remains semantically ordered since it does not involve stored value being subtracted from
         return num1 - num2
       }
   },
     //multiplication
     multiply: function(num1,num2) {
-      return num1 * num2
+      if (num2 === undefined) {
+  //make sure stored value is first
+        calculate.value = calculate.value * num1;
+        return calculate.value
+      }
+      else {
+          return num1 * num2
+        }
     },
-    //division
+  //division
     divide: function(num1,num2) {
-      return num1 / num2
+      if (num2 === undefined) {
+  //put stored valued first
+        calculate.value = calculate.value / num1;
+        return calculate.value
+      }
+      else {
+          return num1 / num2
+        }
     },
-    //exponenets
+  //exponenets
+  //  powerOf: function(num1,num2) {
+    //  return Math.pow(num1, num2);
     powerOf: function(num1,num2) {
-      return Math.pow(num1, num2);
-    },
-
+      if (num2 === undefined) {
+  //code Math.pow of stored value
+        return Math.pow(calculate.value, num1)
+      }
+      else {
+          return Math.pow(num1,num2);
+        }
+    }
 
   };
