f9c682d9fe5705b225fcd431e1bfa450ac0bd39e
michaelmahony <michael.mahony@umbc.edu>
2016-07-05 00:23:50 -0400

    Rewrote functions; JS evidently does not support function overloading.

--- calculator.js
+++ calculator.js
@@ -1,62 +1,66 @@
 var calculator = {
-   var value = 0,
+   value: 0,
 
   clear : function() {
     value = 0;
   },
 
-// Operations with only one argument
-  add : function(x) {
-    value += x;
-    return value;
-  },
-  subtract : function(x) {
-    value -= x;
-    return value;
-  },
-  multiply : function(x) {
-    value *= x;
-    return value;
-  },
-  divide : function(x) {
-    value /= x;
-    return value;
-  },
-  exponent : function(x) {
-    value = Math.pow(value, x);
-    return value;
-  },
-  modulus : function(x) {
-    value = value % x;
-    return value;
+// Note: JS does not support function overloading;
+// Instead, we must manually check the number of and type of arguments
+
+  add : function(x, y) {
+    if (arguments.length == 1) {
+      calculator.value += x;
+      return calculator.value;
+    } else {
+      value = x + y;
+      return x + y;
+    }
   },
 
-// Operations with two arguments
-  add : function(x, y) {
-    value = x + y;
-    return x + y;
-  },
   subtract : function(x, y) {
-    value = x - y;
-    return x - y;
+    if (arguments.length == 1) {
+      calculator.value -= x;
+      return calculator.value;
+    } else {
+      value = x - y;
+      return x - y;
+    }
   },
   multiply : function(x, y) {
-    value = x * y;
-    return x * y;
+    if (arguments.length == 1) {
+      calculator.value *= x;
+      return calculator.value;
+    } else {
+      value = x * y;
+      return x * y;
+    }
   },
-  divide : function(dividend, divisor) {
-    value = x / y;
-    return x / y;
+  divide : function(x, y) {
+    if (arguments.length == 1) {
+      calculator.value /= x;
+      return calculator.value;
+    } else {
+      value = x / y;
+      return x / y;
+    }
   },
-  exponent : function(base, power) {
-    value = Math.pow(base, power);
-    return Math.pow(base, power);
+  exponent : function(x, y) {
+    if (arguments.length == 1) {
+      calculator.value = Math.pow(calculator.value, x);
+      return calculator.value;
+    } else {
+      calculator.value = Math.pow(x, y);
+      return Math.pow(x, y);
+    }
   },
   modulus : function(x, y) {
-    value = x % y;
-    return x % y;
+    if (arguments.length == 1) {
+      calculator.value %= x;
+      return calculator.value;
+    } else {
+      value = x % y;
+      return x % y;
+    }
   },
 }
-
-
-console.log(calculator.exponent(2, 5));
