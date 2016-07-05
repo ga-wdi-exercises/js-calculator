b697fa5ed8896e3fd7916c590e0769445a8613dd
michaelmahony <michael.mahony@umbc.edu>
2016-07-04 23:25:17 -0400

    Created function structures.

---
+++ calculator.js
@@ -0,0 +1,20 @@
+var calculator = {
+  add : function(x, y) {
+
+  }
+  subtract : function(x, y) {
+
+  }
+  multiply : function(x, y) {
+
+  }
+  divide : function(dividend, divisor) {
+
+  }
+  exponent : function(base, power) {
+
+  }
+  modulus : function(base, power) {
+
+  }
+}
