b719a3a0fa46e3cd08609456733e86759ccadf67
GIOMAR SILVA <gsilva23@gmail.com>
2016-07-05 07:15:25 -0400

    Basic calculator

---
+++ calculator.js
@@ -0,0 +1,16 @@
+function add (num1, num2) {
+  return num1 + num2;
+  console.log (num1 + num2);
+}
+function substract (num1, num2){
+  return num1 - num2;
+  console.log (num1 - num2);
+}
+function multiply (num1, num2){
+  return num1 * num2;
+  console.log (num1 * num2);
+}
+function divide (num1, num2){
+  return num1 / num2;
+  console.log (num1 / num2);
+}
