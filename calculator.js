71a1a015c0caf01934ba74939b16da515243c617
michaelmahony <michael.mahony@umbc.edu>
2016-07-04 23:38:11 -0400

    Added persistance of result; Populated calculator functions.

--- calculator.js
+++ calculator.js
@@ -1,20 +1,35 @@
 var calculator = {
+  value = null,
+
+  clear : function() {
+    value = null;
+  },
+
   add : function(x, y) {
-
-  }
+    value = x + y;
+    return x + y;
+  },
   subtract : function(x, y) {
-
-  }
+    value = x - y;
+    return x - y;
+  },
   multiply : function(x, y) {
-
-  }
+    value = x * y;
+    return x * y;
+  },
   divide : function(dividend, divisor) {
-
-  }
+    value = x / y;
+    return x / y;
+  },
   exponent : function(base, power) {
-
-  }
-  modulus : function(base, power) {
-
-  }
+    value = Math.pow(base, power);
+    return Math.pow(base, power);
+  },
+  modulus : function(x, y) {
+    value = x % y;
+    return x % y;
+  },
 }
+
+
+console.log(calculator.exponent(2, 5));
