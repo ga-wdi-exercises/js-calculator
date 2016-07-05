f3ed118e1d13d11d3d0dba8efc8e4dcea7764d97
AlexanderHolland <aholland@mystudybank.com>
2016-07-04 21:55:53 -0400

    alexCalc

---
+++ calculator.js
@@ -0,0 +1,22 @@
+var calculate = {
+  //addition
+   add: function(num1, num2) {
+     return (num1 + num2);
+   },
+  //subtract
+  subtract: function (num1, num2) {
+    return (num1 - num2);
+  },
+  //multiply
+  multiply: function (num1, num2) {
+    return (num1 * num2);
+  },
+  //divide
+  divide: function (num1, num2) {
+    return (num1 / num2);
+  }
+  //exponent
+  exponentiate: function (number, exponent) {
+    return Math.pow(numer, exponent);
+  }
+ };
