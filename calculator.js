28317b0f781d496dcbdd6a41083f199b42af8182
michaelmahony <michael.mahony@umbc.edu>
2016-07-04 23:45:31 -0400

    Created and implemented functions with only one argument operating on the stored value.

--- calculator.js
+++ calculator.js
@@ -1,10 +1,37 @@
 var calculator = {
-  value = null,
+  value = 0,
 
   clear : function() {
-    value = null;
+    value = 0;
   },
 
+// Operations with only one argument
+  add : function(x) {
+    value += x;
+    return value;
+  },
+  subtract : function(x) {
+    value -= x;
+    return value;
+  },
+  multiply : function(x) {
+    value *= x;
+    return value;
+  },
+  divide : function(x) {
+    value /= x;
+    return value;
+  },
+  exponent : function(x) {
+    value = Math.pow(value, x);
+    return value;
+  },
+  modulus : function(x) {
+    value = value % x;
+    return value;
+  },
+
+// Operations with two arguments
   add : function(x, y) {
     value = x + y;
     return x + y;
