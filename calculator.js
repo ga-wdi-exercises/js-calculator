54cd6e199806375173b32a5dedae1154b0544333
Mark Sweet <marks828@gmail.com>
2016-09-10 22:23:16 -0400

    added calculator and index

--- calculator.js
+++ calculator.js
@@ -1,2 +1,22 @@
 var calculator = {
+  add: function (a, b){
+    return a + b;
+  },
+  subtract: function(a, b){
+    return a - b;
+  },
+  multiply: function(a, b){
+    return a * b;
+  },
+  divide: function(a, b){
+    return a / b
+  },
+  exponent: function(a){
+    return a * a
+  },
+  pow: function(a, b){
+    return Math.pow(a, b)
+  }
 }
+
+calculator.add(2, 1)
