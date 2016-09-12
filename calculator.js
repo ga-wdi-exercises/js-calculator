8d8b46235c301bb7afdd69c28349655bd2d7c187
Domonique Griffin <griffind2009@gmail.com>
2016-09-12 17:58:24 -0400

    added object functions

--- calculator.js
+++ calculator.js
@@ -1,2 +1,17 @@
 var calculator = {
+  addition: function(x,y){
+  return x + y
 }
+};
+
+var calculator = {
+subtraction: function(x,y){
+return x - y
+}
+};
+
+var calculator = {
+  multiplication: function(x,y){
+  return x*y
+}
+};
