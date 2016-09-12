e0ae433e24bb64178f9947b57510b80ca2a30a85
Tim Foley <thetimfoley@gmail.com>
2016-09-12 17:03:57 -0400

    basic arithematic

--- calculator.js
+++ calculator.js
@@ -1,2 +1,17 @@
 var calculator = {
+  add: function(a, b) {
+    return a + b;
+  },
+  subtract: function(a,b) {
+    return a - b;
+  },
+  divide: function(a,b) {
+    return a/b;
+  },
+  multiply: function(a,b) {
+    return a*b;
+  },
+  power: function(a,b) {
+    return a**b;
+  }
 }
