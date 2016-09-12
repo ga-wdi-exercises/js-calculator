66e57af3f03af4bc03ec97cfc10e9c2741e08e1b
Latoya Watson <latoyamwatson@gmail.com>
2016-09-12 17:58:19 -0400

    the addition function is  working

--- calculator.js
+++ calculator.js
@@ -1,2 +1,12 @@
+// var calculator = {
+//   add: (x + y),
+//   substract: (x - y),
+//   mult: (x * y),
+//   division: (x/y)
+// }
+
 var calculator = {
+  add: function add(x, y) {
+    return x + y
+  }
 }
