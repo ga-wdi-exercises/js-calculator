26c1ed1309606f96559b4db4be6928dbc37eaf20
Michael Lai <mike24567@gmail.com>
2016-09-12 18:23:11 -0400

    added: add, subtract, multiply, divide, exponent, and sqroot to calculator

--- calculator.js
+++ calculator.js
@@ -1,2 +1,27 @@
 var calculator = {
+  add: function (x,y){
+    console.log(x+y);
+    return(x+y);
+  },
+  subtract: function (x,y){
+    console.log(x-y);
+    return(x-y);
+  },
+  multiply: function (x,y){
+    console.log(x*y);
+    return(x*y);
+  },
+  divide: function (x,y){
+    console.log(x/y);
+    return(x/y);
+  },
+  power: function (x,y){
+    console.log(Math.pow(x,y));
+    return(Math.pow(x,y));
+  },
+  sqroot: function (x){
+    console.log(Math.sqrt(x));
+    return(Math.sqrt(x));
+  }
+
 }
