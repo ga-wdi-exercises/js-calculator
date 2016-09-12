017ebaede33c93e4be45937589852d17659ddfe3
Chris Jaffier <chrisd@Chriss-MacBook-Pro.local>
2016-09-12 19:08:12 -0400

    calcultor complete

--- calculator.js
+++ calculator.js
@@ -1,2 +1,21 @@
 var calculator = {
+
+add: function (x,y) {
+  console.log(x + y);
+},
+ subtract: function (x,y) {
+    console.log(x-y);
+},
+  multiply: function (x,y) {
+    console.log(x*y);
+  },
+  divide: function (x,y){
+  console.log(x/y);
+},
+ exponent: function (x){
+   console.log(x*x);
+},
+triple: function(x){
+  console.log(x*x*x)
+}
 }
