ef26055785e48dc60c66529bfa85a5ae353818de
Khoi Le <khoileee89@gmail.com>
2016-09-12 18:21:46 -0400

    initial calculator

--- calculator.js
+++ calculator.js
@@ -1,2 +1,19 @@
 var calculator = {
+  addition: function(a,b){
+    console.log(a + b)},
+
+
+  subtraction: function(a,b){
+    console.log(a - b)},
+
+  multiplication: function(a,b){
+    console.log(a * b)},
+
+  division: function(a, b){
+    console.log(a / b)},
+
+  exponents: function(a, b){
+    console.log(a**b)},
+addAndMultiply: function(a, b, c){
+  console.log((a + b) / c)}
 }
