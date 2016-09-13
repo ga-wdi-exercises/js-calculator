5c047b1234293cb457ced7af38106744aa1f562e
YOUR FULL NAME <lun.mariancik@gmail.com>
2016-09-13 14:29:20 -0400

    First commit! Modified calculator.js

--- calculator.js
+++ calculator.js
@@ -1,2 +1,34 @@
 var calculator = {
+  value : null,
+  add : function(x, y) {
+     this.value = this.value + x
+      return this.value;
+  },
+  subtract: function(x, y) {
+    this.value = this.value - x
+    return this.value;
+  },
+  divide: function(x, y) {
+    this.value = this.value / x
+    return this.value;
+  },
+  multiply: function(x, y) {
+    this.value = this.value * x
+    return this.value;
+  },
+  sqrt: function(x) {
+    this.value =this.value /Math.sqrt(x)
+    return this.value;
+  },
+  exponentiate: function(x, y) {
+    this.value = this.value ** x
+    return this.value;
+  },
+  clear: function() {
+    this.value = null
+  },
+  master: function(expression) {
+    this.value = eval(expression)
+    return this.value;
+  }
 }
