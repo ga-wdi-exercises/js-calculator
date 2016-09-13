6ee38dbe022908b9231f71d632c71bfc4e4078ee
Michael Lai <mike24567@gmail.com>
2016-09-12 20:03:47 -0400

    Added memory and clear memory

--- calculator.js
+++ calculator.js
@@ -1,25 +1,45 @@
 var calculator = {
-  add: function (x,y){
-    console.log(x+y);
-    return(x+y);
+  value: 0,
+  clear: function(){
+    return calculator.value = 0;  //clear memory//
   },
-  subtract: function (x,y){
+  add: function (x,y){  //addition//
+    console.log(x+y, arguments.length);
+    if (arguments.length ==2){
+      return calculator.value = (x+y);
+    }  else {
+      return calculator.value = (calculator.value + x);
+    }
+  },
+  subtract: function (x,y){  //subtraction//
     console.log(x-y);
-    return(x-y);
+    if (arguments.length ==2){
+      return calculator.value = (x-y);
+    }  else {
+      return calculator.value = (calculator.value - x);
+    }
   },
-  multiply: function (x,y){
+  multiply: function (x,y){  //multiplication//
     console.log(x*y);
-    return(x*y);
+    if (arguments.length ==2){
+      return calculator.value = (x*y);
+    }  else {
+      return calculator.value = (calculator.value * x);
+    }
   },
-  divide: function (x,y){
+  divide: function (x,y){  //division//
     console.log(x/y);
-    return(x/y);
+    if (arguments.length ==2){
+      return calculator.value = (x/y);
+    }  else {
+      return calculator.value = (calculator.value / x);
+    }
   },
-  power: function (x,y){
+  power: function (x,y){  //exponent//
     console.log(Math.pow(x,y));
     return(Math.pow(x,y));
   },
-  sqroot: function (x){
+  sqroot: function (x){  //square root//
     console.log(Math.sqrt(x));
     return(Math.sqrt(x));
   }
