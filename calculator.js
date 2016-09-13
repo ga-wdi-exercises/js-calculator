0d6f5fe1597b646a29558a5d0bd1ca23303392f3
Liza Floyd <lizafloyd@gmail.com>
2016-09-13 14:04:29 -0400

    calculator and Bonus 1 complete

--- calculator.js
+++ calculator.js
@@ -1,2 +1,33 @@
-var calculator = {
+
+var z = 0;
+var calc = {
+  add:function(y){
+    z= z+y
+    return z
+  },
+  sub:function(y){
+    z= z-y
+    return z
+  },
+  mult:function(y){
+    z= z*y
+    return z;
+  },
+  div:function(y){
+    z= z/y
+    return z;
+  },
+  exp:function(y){
+    z= Math.pow(z,y)
+    return z
+  },
+  squ:function(){
+    z =z*z
+    return z
+  },
+  value: z,
+  clear:function(){
+    z=0
+    return z
+  }
 }
