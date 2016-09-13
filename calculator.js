580faf045a4f97987ce7e9779be54cf6d3cf6fc2
Alex Manno <alman92@gmail.com>
2016-09-13 12:23:40 -0400

    Basic Functions

--- calculator.js
+++ calculator.js
@@ -1,2 +1,22 @@
 var calculator = {
+  add:function (x , y){
+    console.log(x + y);
+    return (x + y);
+  },
+  sub:function (x , y){
+    console.log(x - y);
+    return (x - y);
+  },
+  multiply:function (x , y){
+    console.log(x * y);
+    return (x * y);
+  },
+  divide:function (x , y){
+    console.log(x / y);
+    return (x / y);
+  },
+  // pow:function (x , y){
+  //   console.log(pow(x,y));
+  //   return (pow(x,y));
+  // }
 }
