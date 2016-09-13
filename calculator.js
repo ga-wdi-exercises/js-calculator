d0c164667d721c8e77274fa339d4f68c29a81216
Tim Foley <thetimfoley@gmail.com>
2016-09-13 09:45:48 -0400

    added comment

--- calculator.js
+++ calculator.js
@@ -1,6 +1,7 @@
 var calculator = {
   value: 0,
   clear: function() {
+    //do I need to use `this.` before value each time? I tried taking it out but got `value is not defined`, but I saw in another student's that just plain `value = 0` can work?
     this.value = 0;
     return this.value;
   },
