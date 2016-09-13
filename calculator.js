0f9c78fe61802bfe610e07358d967ad273151a7b
Kevin Mahoney <kjmahoney13@gmail.com>
2016-09-13 19:46:30 -0400

    Completed bonus 1

--- calculator.js
+++ calculator.js
@@ -1,27 +1,58 @@
-
-var car = {
-  make: "Honda",
-  model: "Civic",
-  year: 1997
-}
-
 var calculator = {
+
+  value: 0,
+
   add: function(x,y){
-    return x+y;
-  },
-  subtraction: function(x,y){
-    return x-y;
-  },
+    if (arguments.length == 2) {
+        return calculator.value = x+y;
+    }
+    else{
+        return calculator.value = (calculator.value + x);
+  }
+},
+  subtraction: function(x,y) {
+    if (arguments.length == 2) {
+        return calculator.value = x-y;
+    }
+    else{
+        return calculator.value = (calculator.value - x);
+  }
+},
   multiplication: function(x,y){
-    return x*y;
+    if (arguments.length == 2) {
+        return calculator.value = x*y;
+    }
+    else{
+        return calculator.value = (calculator.value * x);
+      }
+
   },
   division: function(x,y){
-    return x/y;
+    if (arguments.length == 2) {
+        return calculator.value = x/y;
+    }
+    else{
+        return calculator.value = (calculator.value / x);
+      }
+
   },
   exponents: function(x,y){
-    return x**y;
+    if (arguments.length == 2) {
+        return calculator.value = x**y;
+    }
+    else{
+        return calculator.value = (calculator.value ** x);
+      }
+
   },
   squareRoot: function(x,y){
-    return Math.sqrt(x);
+    if (arguments.length == 0) {
+        return calculator.value = Math.sqrt(x);
+        alert("Only one argument can be entered for the square root function");
+    }
+    else{
+        return calculator.value = Math.sqrt(x);
+
   }
+}
 };
