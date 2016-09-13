81be26c10b01371cb74be1f3f7a97d0d42c547a3
Domonique Griffin <griffind2009@gmail.com>
2016-09-13 13:33:37 -0400

    Made into an object

--- calculator.js
+++ calculator.js
@@ -1,32 +1,19 @@
 var calculator = {
   addition: function(x,y){
   return x + y
-}
-};
-
-var calculator = {
+},
 subtraction: function(x,y){
 return x - y
-}
-};
-
-var calculator = {
+},
   multiplication: function(x,y){
   return x*y
-}
-};
-
-var calculator = {
+},
   division: function(x,y) {
     return x/y
-  }
-};
-var calculator = {
+},
   exponents: function(x,y){
     return x**y
-  }
-};
-var calculator = {
+},
   cubed: function(x) {
     return x*x*x
   }
