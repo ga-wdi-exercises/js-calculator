f1f84cdad6e5a3e9590ff2bcc516ca6ec23dbec0
Andrew Whitley <awhitley1233@gmail.com>
2016-09-12 23:04:48 -0400

    calculator has memory, can perist calculations, can clear memory

--- calculator.js
+++ calculator.js
@@ -1,22 +1,34 @@
 console.log("working")
 
+
+
 var calculator = {
+  value : null,
   add : function(x, y) {
-    return (x + y)
+    value = (typeof y !== 'undefined')? x + y : value + x
+    return value
   },
   subtract : function(x, y) {
-    return(x - y)
+    value = (typeof y !== 'undefined')? x - y : value - x
+    return value
   },
   multiply : function(x, y) {
-    return(x * y)
+    value = (typeof y !== 'undefined')? x * y : value * x
+    return value
   },
   divide : function(x, y) {
-    return(x / y)
+    value = (typeof y !== 'undefined')? x / y : value / x
+    return value
   },
   exponent : function(x, y) {
-    return Math.pow(x, y)
+    value = (typeof y !== 'undefined')? Math.pow(x, y) : Math.pow(value, x)
+    return value
   },
   root : function(x, y) {
-    return Math.pow(x, 1/y)
+    value = (typeof y !== 'undefined')? Math.pow(x, 1/y) : Math.pow(value, 1/x)
+    return value
+  },
+  clear : function() {
+    value = null
   }
 }
