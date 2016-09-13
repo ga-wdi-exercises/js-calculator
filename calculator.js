0d820e8111a13caed29ecce15c24f5ce6b8e3a0b
Andrew Whitley <awhitley1233@gmail.com>
2016-09-12 21:16:02 -0400

    created calculator object; defined arithmatic

--- calculator.js
+++ calculator.js
@@ -1,2 +1,22 @@
+console.log("working")
+
 var calculator = {
+  add : function(x, y) {
+    return (x + y)
+  },
+  subtract : function(x, y) {
+    return(x - y)
+  },
+  multiply : function(x, y) {
+    return(x * y)
+  },
+  divide : function(x, y) {
+    return(x / y)
+  },
+  exponent : function(x, y) {
+    return Math.pow(x, y)
+  },
+  root : function(x, y) {
+    return Math.pow(x, 1/y)
+  }
 }
