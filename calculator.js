e5499f0db3489a34da6cd16a989c0d9d3e4abb64
Alexander <ajspottswood@gmail.com>
2016-09-13 19:29:25 -0400

    Revert "created calculator functionality"
    This reverts commit 01c753158d377d83928ece8edbac485496f97a22.

--- calculator.js
+++ calculator.js
@@ -1,26 +1,2 @@
 var calculator = {
-add:function (num1, num2){
-  console.log(num1 + num2);
-},
-subtract:function (num1, num2){
-  console.log(num1 - num2);
-  return (num1 - num2);
-},
-multiply:function (num1, num2){
-  console.log(num1 * num2);
-  return (num1 * num2);
-},
-divide:function (num1, num2){
-  console.log(num1 / num2);
-  return (num1 / num2);
-},
-exponent:function (num1, num2){
-  console.log(Math.pow(num1, num2));
-  return(Math.pow(num1, num2));
-},
-root:function (num1){
-  console.log(Math.sqrt(num1));
-  return(Math.sqrt(num1));
-}
-
 }
