01a4beb71d17ce9d5b6c192fcbe032bc94e64d56
Poh Chuan Kuan <pohckuan@yahoo.com>
2016-09-13 10:02:16 -0400

    bonus1 without clear method

---
+++ bonus1.js
@@ -0,0 +1,18 @@
+var value = 0
+var calculator = {
+  add: function(x){
+return  value = value + x},
+subtract: function(x){
+  return value = value- x },
+multiply: function(x){
+  return value = value * x},
+divide: function(x){
+  return value = value / x
+},
+exponent: function(x){
+  return value = value ** x
+},
+remainder: fucntion(x){
+  return value = value % x
+}
+}
