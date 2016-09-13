51dc525b235fcd9a099d44e2cfcdbdaaf4e29cbc
Poh Chuan Kuan <pohckuan@yahoo.com>
2016-09-13 09:07:01 -0400

    #1

--- calculator.js
+++ calculator.js
@@ -1,2 +1,14 @@
 var calculator = {
-}
+  add: function(x,y){
+  return x + y},
+  subtract: function(x,y){
+    return x - y },
+  multiply: function(x,y){
+    return x * y},
+  divide: function(x,y){
+    return x / y},
+  exponent: function(x,y){
+    return x ** y},
+  remainder: function(x,y){
+    return x % y}
+};
