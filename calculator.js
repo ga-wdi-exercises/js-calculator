34019779a19206d5568f7286a94d458aac4ad13a
brown2206 <brown.angelo.jr@gmail.com>
2016-09-13 01:20:21 -0400

    created js calculator objects with remainder

--- calculator.js
+++ calculator.js
@@ -1,2 +1,26 @@
 var calculator = {
+  add: function(x,y) {
+    console.log(x+y);
+    return x+y;
+  },
+  sub: function(x,y){
+    console.log(x-y);
+    return x-y;
+  },
+  mult: function(x,y){
+    console.log(x*y);
+    return x*y;
+  },
+  divide: function(x,y){
+    console.log(x/y);
+    return x/y;
+  },
+  exponent: function(x,y){
+    console.log(x**y);
+    return x**y;
+  },
+  remainder: function(x,y){
+    console.log(x%y);
+    return x%y;
+  }
 }
