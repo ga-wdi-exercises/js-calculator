eb79284597f05b95ecbbf4e6490fcf9b3df7f2c1
William Keating <keating49@gmail.com>
2016-09-13 14:40:09 -0400

    needs sqrt

--- calculator.js
+++ calculator.js
@@ -1,2 +1,22 @@
 var calculator = {
+  add: function ( num1, num2 ){
+    console.log( num1 + num2 );
+    return num1 + num2;
+  },
+  square: function ( num1, num2 ){
+    console.log(num1 ** num2 );
+    return num1 ** num2;
+  },
+  subtract: function ( num1, num2 ){
+    console.log( num1 - num2 );
+    return num1 - num2;
+  },
+  multiply: function ( num1, num2 ){
+    console.log( num1 * num2 );
+    return num1 * num2;
+  },
+  divide: function ( num1, num2 ){
+    console.log( num1 / num2 );
+    return num1 / num2;
+  }
 }
