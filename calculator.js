705d861afa8af1bbad4572b2ff67a0f55167dd7d
Kevin Mahoney <kjmahoney13@gmail.com>
2016-09-13 18:18:13 -0400

    made calculator logic

--- calculator.js
+++ calculator.js
@@ -1,2 +1,27 @@
-var calculator = {
+
+var car = {
+  make: "Honda",
+  model: "Civic",
+  year: 1997
 }
+
+var calculator = {
+  add: function(x,y){
+    return x+y;
+  },
+  subtraction: function(x,y){
+    return x-y;
+  },
+  multiplication: function(x,y){
+    return x*y;
+  },
+  division: function(x,y){
+    return x/y;
+  },
+  exponents: function(x,y){
+    return x**y;
+  },
+  squareRoot: function(x,y){
+    return Math.sqrt(x);
+  }
+};
