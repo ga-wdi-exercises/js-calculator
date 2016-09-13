d159dab20e2400107fe5e189ccff2f94fa574124
Latoya Watson <latoyamwatson@gmail.com>
2016-09-13 14:31:25 -0400

    The calculator worksgit add .

--- calculator.js
+++ calculator.js
@@ -1,12 +1,19 @@
-// var calculator = {
-//   add: (x + y),
-//   substract: (x - y),
-//   mult: (x * y),
-//   division: (x/y)
-// }
+confirm("I am working!");
 
 var calculator = {
+  division: function divison(x, y) {
+    return x / y;
+  },
+  mult: function mult(x, y) {
+    return x * y;
+  },
+  sub: function sub(x, y) {
+    return x - y;
+  },
   add: function add(x, y) {
-    return x + y
+    return x + y;
   }
 }
+
+// calculator.mult(2, 2)
+// 4
