9786499909bc300a0f4728cd1b383a68a0cd7dde
Alexander <ajspottswood@gmail.com>
2016-09-13 21:35:31 -0400

    created memory

--- calculator.js
+++ calculator.js
@@ -1,27 +1,28 @@
 var calculator = {
+value:null,
 add:function (num1, num2){
-  console.log(num1 + num2);
-  return (num1 + num2);
+  value = (num1 + num2);
+  return value;
 },
 subtract:function (num1, num2){
-  console.log(num1 - num2);
-  return (num1 - num2);
+  value = (num1 - num2);
+  return value;
 },
 multiply:function (num1, num2){
-  console.log(num1 * num2);
-  return (num1 * num2);
+  value = (num1 * num2);
+  return value;
 },
 divide:function (num1, num2){
-  console.log(num1 / num2);
-  return (num1 / num2);
+  value = (num1 / num2);
+  return value;
 },
 exponent:function (num1, num2){
-  console.log(Math.pow(num1, num2));
-  return(Math.pow(num1, num2));
+  value = (Math.pow(num1, num2));
+  return value;
 },
 root:function (num1){
-  console.log(Math.sqrt(num1));
-  return(Math.sqrt(num1));
+  value = (Math.sqrt(num1));
+  return value;
 }
 
 }
