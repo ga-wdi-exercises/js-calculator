2e0a7a19ef6ac4108060dee602d2ff1bbf4130d5
manish pradhan <pradhanmanish33@gmail.com>
2016-09-14 18:52:49 -0400

    initial commit

--- calculator.js
+++ calculator.js
@@ -1,2 +1,22 @@
+
 var calculator = {
-}
+  add:function(x, y){
+      return x + y; },
+
+  subtract:function(x, y){
+           return x - y; },
+
+  multiply:function(x, y) {
+           return x * y; },
+
+  devide:function(x, y) {
+         return x / y; },
+
+         factor:function Factorial (n) {
+           if (n <= 1) {
+               return 1;
+           }
+           return n * Factorial (n-1);
+       }
+
+};
