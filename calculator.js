4040341d2c745e68e78a1735906950f9099618b5
Doray Gisele Owens <doray.owens@gmail.com>
2016-09-14 20:35:42 -0400

    js-calculator

--- calculator.js
+++ calculator.js
@@ -1,2 +1,32 @@
 var calculator = {
 }
+var num1,num2,result;
+function setValues()
+{
+  num1 = Number(document.getElementById("num1").value);
+  num2 = Number(document.getElementById("num2").value);
+}
+function sum()
+{
+  setValues();
+  result = num1+num2;
+  alert("Total:"+result)
+}
+function subtract()
+{
+  setValues();
+  result = num1-num2;
+  alert("Total:"+result)
+}
+function mult()
+{
+  setValues();
+  result = num1*num2;
+  alert("Total:"+result)
+}
+function div()
+{
+  setValues();
+  result = num1/num2;
+  alert("Total:"+result)
+}
