9e93930c78c07e484ad2dcdec2aa0f2330e6932c
Ian Hollands <ihollands@outlook.com>
2016-09-14 20:32:50 -0400

    calculator without bonuses

--- calculator.js
+++ calculator.js
@@ -1,2 +1,23 @@
+
+
 var calculator = {
+  value: 0,
+  add: function (x,y) {
+      return x + y;
+    },
+  subtract: function(x,y) {
+    return x - y;
+  },
+  multiply: function(x,y) {
+    return x * y;
+  },
+  divide: function(x,y) {
+    return x / y;
+  },
+  exponent: function(x,y) {
+    return Math.pow(x,y);
+  },
+  modulus: function(x,y) {
+    return x % y;
+  }
 }
