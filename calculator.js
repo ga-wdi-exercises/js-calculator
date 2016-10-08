63d85ab19d2f86e8291d2e38f10ee0f4f6b9a31c
AdrienneD <adriennedao@gmail.com>
2016-10-08 18:19:50 -0400

    initial commit

--- calculator.js
+++ calculator.js
@@ -1,2 +1,23 @@
-var calculator = {
+var calculator ={
+  add: function(num1, num2){
+  console.log(num1+ num2);},
+
+multiply: function(num1, num2){
+  console.log(num1*num2);},
+
+
+subtract: function(num1, num2){
+  console.log(num1-num2);},
+
+
+division: function(num1, num2){
+  console.log(num1/num2);},
+
+
+multiply: function(num1, num2){
+  console.log(num1*num2);},
+
+
+exponent: function(num1, num2){
+  console.log(num1%num2);},
 }
