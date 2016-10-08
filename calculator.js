acc8fc9fc97eac095a3f1685796f839f3fc2be69
Angel Valant <angelgithub@gmail.com>
2016-10-08 16:07:31 -0400

    updates calculator.js

--- calculator.js
+++ calculator.js
@@ -1,2 +1,16 @@
+console.log("Please select an operator: .add, .subtract, .multiply, .division, .exponents, .remainder!")
+
 var calculator = {
+  add: function(x,y){
+    console.log(x+y);},
+  subtract: function(x,y){
+    console.log(x-y);},
+  multiply: function(x,y){
+    console.log(x*y);},
+  division: function(x,y){
+    console.log(x/y);},
+  exponents: function(x,y){
+    console.log(Math.pow(x,y));},
+    remainder: function(x,y){
+      console.log(x%y);},
 }
