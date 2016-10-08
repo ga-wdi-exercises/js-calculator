d511b841052a3559f7f908511b68b0a2c551adb3
Rob Romaniw <robertromaniw@gmail.com>
2016-10-08 14:37:49 -0400

    Initial Commit

--- calculator.js
+++ calculator.js
@@ -1,2 +1,22 @@
+
+
 var calculator = {
-}
+  square: function(num){
+  return num * num;
+  },
+  multiply: function(num1, num2) {
+      return num1 * num2;
+  },
+  add: function(num1, num2) {
+      return num1 + num2;
+  },
+  subtract: function(num1, num2) {
+      return num1 - num2;
+  },
+  divide: function(num1, num2) {
+      return num1 / num2;
+  },
+  raisePower: function(num1, num2) {
+      return Math.pow(num1, num2);
+    },
+  };
