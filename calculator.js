8ae43220b550608a2bc673b52211248a6255538d
YOUR FULL NAME <THE_EMAIL_YOU_USE_FOR_GITHUB@EMAIL.COM>
2016-10-09 01:15:37 -0700

    Vanessa's-JSCalc

--- calculator.js
+++ calculator.js
@@ -1,2 +1,32 @@
 var calculator = {
-}
+  add: function(x,y) {
+    console.log(x+y);
+    return x+y;
+  },
+  subtract: function(x,y) {
+    console.log(x-y);
+    return x-y;
+  },
+  multiply: function(x,y) {
+    console.log(x*y);
+    return x*y;
+  },
+  divide: function(x,y) {
+    console.log(x/y);
+    return x/y;
+  }
+  exponent: Math.pow(x,y) {
+    console.log(x^,y^);
+    return x^y^;
+  },
+  remainder: function(x,y) {
+    console.log(x%y);
+    return(x%y);
+  }
+// var remValue = (x,y)------> bonus
+// document.getElementById("display").value=0;
+//
+// }
+// function Clear() {------>bonus
+//   document.getElementById("screen").innerHTML ="0";
+// } (I tried with this, just goggled up some things and came up w/this)
