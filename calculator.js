1497684d5aecd1b44dad7e9390ed5a353aa53e5c
Robert Zehnder <robzcodes@gmail.com>
2016-10-09 15:01:25 -0400

    Rob Z HW: completes JS Calculator

--- calculator.js
+++ calculator.js
@@ -1,2 +1,82 @@
-var calculator = {
+
+
+var calcValue= '';
+var result = "";
+var calculation = '';
+
+function addOperator(operator){
+  addResult();
+  calculation += operator;
+  document.getElementById('result').innerHTML = calculation;
+  document.getElementById('entry').value = '';
 }
+
+function addResult () {
+  calculation += calcValue;
+  document.getElementById('result').innerHTML = calculation;
+  calcValue = '';
+}
+
+//Functions-------------------------------------------------------
+
+function startCalculator () {
+  calcValue = document.getElementById('entry').value;
+  document.getElementById('result').innerHTML = 'Result';
+}
+
+function addValue (number) {
+
+  if (calcValue == '0'){
+    calcValue = "";
+  }
+
+  calcValue += number;
+  document.getElementById('entry').value = calcValue;
+}
+
+function clearCalculator() {
+  calcValue = "";
+  result = "";
+  calculation = ''
+  document.getElementById('entry').value = calcValue;
+  document.getElementById('result').innerHTML = '';
+}
+
+function solve() {
+  addResult();
+  var oldResult = calculation;
+  document.getElementById('result').innerHTML = oldResult + ' = ' + eval(calculation);
+  calculation = eval(calculation);
+}
+
+
+// function addResult () {
+//
+// }
+
+// var calculator = {
+  // add:
+
+  // function additionFunc() {
+  //
+  // }
+  //
+  // // subtract:
+  // function subtractionFunc() {
+  //
+  // }
+  //
+  // // multiply:
+  // function multiplicationFunc() {
+  //   addResult();
+  //   result += '*';
+  //   eval(result);
+  //   document.getElementById('result').innerHTML = result;
+  //   document.getElementById('entry').value = '';
+  // }
+  //
+  // // divide:
+  // function divisionFunc() {
+  //
+  // }
+// }
