35ce522e13f372d7c38358a21c46a8b9334d6867
robert <robert.k25@gmail.com>
2016-10-09 20:03:28 -0400

    barebones calculator

--- calculator.js
+++ calculator.js
@@ -1,2 +1,14 @@
-var calculator = {
-}
+var calculator =  {
+  add: (num1,num2) => num1 + num2,
+  subtract: (num1, num2) => num1 - num2,
+  divide: (num1, num2) => num1 /num2,
+  exponent: (num1, num2) => Math.pow(num1, num2),
+  double: (num) => num * 2
+};
+
+
+// console.log(calculator.add(2,1));
+// console.log(calculator.subtract(10,1));
+// console.log(calculator.divide(10,2));
+// console.log(calculator.exponent(2,3));
+// console.log(calculator.double(16));
