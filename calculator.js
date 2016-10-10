79e367e977bc140333442aede9ab8fe67c2e929c
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2016-10-09 20:47:21 -0400

    created required calculator functions

--- calculator.js
+++ calculator.js
@@ -1,2 +1,30 @@
-var calculator = {
+
+var value = 0;
+var calculator = function operator(value){
+
+function add(x,y) {
+  console.log(value(add));
+  return value (x+=y);
 }
+
+function substract(x,y) {
+  console.log(value(subtract));
+  return value (x-=y);
+}
+
+function multiply(x,y) {
+  console.log(value(multiply));
+  return value (x*=y);
+}
+
+function divide(x,y) {
+  console.log(value(divide));
+  return value (x/=y);
+}
+
+Math.exp(x,y); {
+  console.log(value(Math.exp));
+  return value(Math.exp(x,y));
+}
+
+};
