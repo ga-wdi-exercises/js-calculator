628190b948d072ae113d20885b4327c91b208ebd
Jonathan Reiser <jlr9v@virginia.edu>
2016-10-09 21:35:23 -0400

    Updates calculator.js

--- calculator.js
+++ calculator.js
@@ -1,2 +1,31 @@
+var value = 0;
 var calculator = {
+  add: function(a,b){
+    value = a + b
+    return value
+  },
+  subtract: function(a,b){
+    value = a - b
+    return value
+  },
+  multiply: function(a,b){
+    value = a * b
+    return value
+  },
+  divide: function(a,b){
+    value = a / b
+    return value
+  },
+  exponents: function(a,b){
+    value = Math.pow(a,b)
+    return value
+  },
+  squareRoot: function(a){
+    value = Math.sqrt(a)
+    return value
+  },
+  clear: function(){
+    value = 0
+    return value
+  }
 }
