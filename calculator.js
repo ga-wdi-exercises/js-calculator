a527befe086229e4bfabe9ed0afd1eb5da265b63
Donovan Allen <dallen.928@gmail.com>
2016-10-10 08:49:14 -0400

    transferred work from js-calc un-forked dir to submit

--- calculator.js
+++ calculator.js
@@ -1,2 +1,31 @@
 var calculator = {
+  function add (num1,num2) {
+    console.log(num1 + num2)
+    return (num1 + num2);
+  }
+
+  function subtract (num1, num2) {
+    console.log(num1 - num2);
+    return (num1 - num2);
+  }
+
+  function multiply (num1, num2) {
+    console.log(num1 * num2);
+    return (num1 * num2);
+  }
+
+  function divide (num1, num2) {
+    console.log(num1 / num2);
+    return (num1 / num2);
+  }
+
+  function power (num1, num2) {
+    console.log(math.pow(num1, num2));
+    return (math.pow(num1, num2);
+  }
+
+  function remainder (num1, num2) {
+    console.log(num1 % num2);
+    return (num1 % num2);
+  }
 }
