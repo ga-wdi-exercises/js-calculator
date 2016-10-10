63406bde3bb4c82f4e55f4e8877fb83f36f430e7
robert <robert.k25@gmail.com>
2016-10-10 08:51:51 -0400

    fixed missing operations

--- calculator.js
+++ calculator.js
@@ -2,8 +2,9 @@ var calculator =  {
   add: (num1,num2) => num1 + num2,
   subtract: (num1, num2) => num1 - num2,
   divide: (num1, num2) => num1 /num2,
-  exponent: (num1, num2) => Math.pow(num1, num2),
-  double: (num) => num * 2
+  exponentiate: (num1, num2) => Math.pow(num1, num2),
+  double: (num) => num * 2,
+  multiply: (num1, num2) => num1 * num2
 };
 
 
