6c4f61320b45d8e3fb13f9d1eb7a0d5cf8eb1c13
Travis Batts <Travisbatts6@gmail.com>
2016-10-12 08:51:50 -0400

    calculator

--- calculator.js
+++ calculator.js
@@ -1,2 +1,27 @@
+// var calculator = {
+//
+// function add(num1, num2){
+//   console.log(num1+num2)},
+// }
+
+
+
+
 var calculator = {
-}
+  add: function(x,y) {
+  console.log(x+y)},
+
+  sub: function(x,y) {
+  console.log(x+y)},
+
+  Multiply: function(x,y) {
+  console.log(x+y)},
+
+
+  divide: function(x,y) {
+  console.log(x+y)},
+
+  math.power: function(xy, {
+    console.log(x+y)}
+
+ }
