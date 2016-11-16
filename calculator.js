bb9d4c9a29b96e37289dbbcb3973a5c8b4ace4a0
mwb86 <masonwbrown@gmail.com>
2016-11-16 14:59:28 -0500

    basic calculator with no memory

--- calculator.js
+++ calculator.js
@@ -1,2 +1,20 @@
+
 var calculator = {
+
+addition: function(x,y){
+  return (x+y);
+},
+subtraction: function(x,y){
+  return (x-y);
+},
+multiplication: function(x,y){
+  return (x*y);
+},
+division: function(x,y){
+  return (x/y);
+},
+exponents: function(x,y){
+  return Math.pow(x,y);
 }
+
+};
