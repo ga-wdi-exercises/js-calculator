36413415833e8bc78d2da8a6274494354bedac52
alyssafelix7 <alyssa.felix7@gmail.com>
2016-11-16 14:55:47 -0500

    Create add key, value set to method, addition

--- calculator.js
+++ calculator.js
@@ -1,2 +1,12 @@
-var calculator = {
-}
+// Create an object
+let calculator = {
+  // Addition
+  add: function(a, b) {
+    return a + b;
+  },
+  // Subtraction
+  // Multiplication
+  // Division
+  // Exponents (Hint: Look up Javascript's Math object)
+  // An operation of your choice!
+};
