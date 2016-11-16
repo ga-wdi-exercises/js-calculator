6c6f22990a00f9d46acb7a37c32fa378eea5c489
yourUsername <your_email@example.com>
2016-11-16 15:04:36 -0500

    Add calculator functions to js

--- calculator.js
+++ calculator.js
@@ -1,2 +1,32 @@
 var calculator = {
-}
+//Math functions
+  add: function(a,b){
+      console.log(a + b);
+    },
+  subtract: function(a,b){
+      console.log(a - b);
+    },
+  multiply: function(a,b){
+      console.log(a * b);
+    },
+  division: function(a,b){
+      console.log(a / b);
+    },
+  exponent: function(a,b){
+      console.log(Math.pow(a,b));
+    },
+  mod: function (a,b){
+      console.log(a%b);
+  },
+};
+
+
+
+
+//create array for calculator functions
+/*Addition
+Subtraction
+Multiplication
+Division
+Exponents (Hint: Look up Javascript's Math object)
+An operation of your choice!*/
