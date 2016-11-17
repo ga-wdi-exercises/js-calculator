1f25801d9f23ad09d77a68857c8463835a4dec9f
yourUsername <your_email@example.com>
2016-11-16 23:01:58 -0500

    started calc in .js notated

--- calculator.js
+++ calculator.js
@@ -1,2 +1,68 @@
-var calculator = {
-}
+/*The calculator is the variable because it does not change but the keys: add;
+subtract; multiply; divide do because we want them to do something*/
+
+/*  Nov. 16, 2016 Wed. 10:52
+Worked on another method with Ara and Anu earlier in class, but I am implementing
+my thinking and what we applied in class by myself. Within the last hour
+I have been testing my .js in the console with no avail to getting what I want.
+I will continue in the morning and ask questions, collaborate and use additional
+resources (Google, notes, etc..). Before leaving I looked at it again and
+Googled 'write a function to add two numbers javascript' which brought me closer
+and I feel confident in the change that I made, but unsure of what to input
+in the console.*/
+
+
+
+
+let calculator = {
+  a = 0;
+  b = 1;
+  add: evaluate = number(a) + number(b);
+
+    }
+
+  },
+
+  subtract: function () {
+
+  },
+
+  multiply: function (){
+
+  },
+
+  divide: function (){
+
+  },
+
+
+},
+
+
+
+
+
+
+
+
+
+
+/*let car = {
+  make: "honda",
+  model: "civic",
+  year: "1997",
+} */
+
+
+
+/*let result = 0;
+
+    add: function (a, b = 0) {
+    result = a + b;
+    }
+
+    multiply:
+    subtract:
+    divide:
+
+}*/
