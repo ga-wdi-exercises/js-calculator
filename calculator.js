6b1ed3fff37d67ad8dfaab50aa50d1e59924d08c
alyssafelix7 <alyssa.felix7@gmail.com>
2016-11-16 19:37:29 -0500

    Create for loop that iterates through keys
    - Create onclick function for keys
    - Appends btn value to .screen div

--- calculator.js
+++ calculator.js
@@ -23,3 +23,27 @@ let calculator = {
   // An operation of your choice!
 
 };
+
+// Create keys variable, stores all calculator span tags
+const keys = document.querySelectorAll("#calculator span");
+
+
+for(let i = 0; i < keys.length; i++){
+  keys[i].onclick = function(e){
+    // Select .screen element and set to input
+    let input = document.querySelector(".screen");
+    //let inputVal = input.innerHTML;
+    // Set keys click value to btnVal variable
+    let btnVal = this.innerHTML;
+    // Append btnVal value to input html
+    input.innerHTML += btnVal;
+
+    // switch (expression) {
+    //   case expression:
+    //
+    //     break;
+    //   default:
+    //
+    // }
+  };
+}
