3e66e9a8639921d8ff3892ec3ab165bd57f34972
erikslewis <Adelina198527@gmail.com>
2016-11-17 16:21:26 -0500

    almost

--- calculator.js
+++ calculator.js
@@ -1,2 +1,32 @@
+var Display= "number";
+var Memory  = "0";
+var   Current = "0";
+var   Operation = 0;
+const   MAXLENGTH = 30;
+
 var calculator = {
 }
+
+calculator = {
+  add: function (a, b) {
+    return a + b;
+  },
+  subtract: function (a, b) {
+    return b - a;
+  },
+  multiply: function (a, b) {
+    return a * b;
+  },
+  divide: function (a, b) {
+    return a / b;
+  },
+  exponentiate: function (a, b) {
+    return Math.pow(a, b);
+  }
+};
+
+console.log(String(calculator.add(4, 5)));
+console.log(String(calculator.subtract(4, 5)));
+console.log(String(calculator.multiply(4, 5)));
+console.log(String(calculator.divide(4, 5)));
+console.log(String(calculator.exponentiate(4, 5)));
