cbb2e887c9d3e015d5f6560825cb561b82f96ffb
yourUsername <your_email@example.com>
2016-11-20 19:40:10 -0500

    Complete calculator HW

--- calculator.js
+++ calculator.js
@@ -1,32 +1,87 @@
-var calculator = {
-//Math functions
-  add: function(a,b){
-      console.log(a + b);
-    },
-  subtract: function(a,b){
-      console.log(a - b);
-    },
-  multiply: function(a,b){
-      console.log(a * b);
-    },
-  division: function(a,b){
-      console.log(a / b);
-    },
-  exponent: function(a,b){
-      console.log(Math.pow(a,b));
-    },
-  mod: function (a,b){
-      console.log(a%b);
+// let calculator = {
+// //Math functions
+// value:
+//
+//   add: function(a,b){
+//       console.log(a + b);
+//     },
+//   subtract: function(a,b){
+//       console.log(a - b);
+//     },
+//   multiply: function(a,b){
+//       console.log(a * b);
+//     },
+//   division: function(a,b){
+//       console.log(a / b);
+//     },
+//   exponent: function(a,b){
+//       console.log(Math.pow(a,b));
+//     },
+//   mod: function (a,b){
+//       console.log(a%b);
+//   },
+// };
+
+let calculator = {
+  value: 0,
+
+  clear: this.value = 0,
+
+  master:
+
+  add: function(param1,param2) {
+    if (arguments.length === 2) {
+      this.value = (param1 + param2);
+      console.log(this.value);
+    } else {
+       this.value = this.value + param1;
+      console.log(this.value);
+    }
   },
+  subtract: function(param1,param2) {
+    if (arguments.length === 2) {
+      this.value = (param1 - param2);
+      console.log(this.value);
+    } else {
+       this.value = this.value - param1;
+      console.log(this.value);
+    }
+  },
+  multiply: function(param1,param2) {
+    if (arguments.length === 2) {
+      this.value = (param1 * param2);
+      console.log(this.value);
+    } else {
+       this.value = this.value * param1;
+      console.log(this.value);
+    }
+  },
+  divide: function(param1,param2) {
+    if (arguments.length === 2) {
+      this.value = (param1 / param2);
+      console.log(this.value);
+    } else {
+       this.value = this.value / param1;
+      console.log(this.value);
+    }
+  },
+  square: function(param1,param2) {
+    if (arguments.length === 2) {
+      this.value = math.pow(param1,param2);
+      console.log(this.value);
+    } else {
+      this.value = math.pow(this.value,param1);
+      console.log(this.value);
+    }
+  },
+  mod: function(param1,param2) {
+    if (arguments.length === 2) {
+      this.value = (param1 % param2);
+      console.log(this.value);
+    } else {
+       this.value = this.value % param1;
+      console.log(this.value);
+    }
+  },
+
 };
-
-
-
-
-//create array for calculator functions
-/*Addition
-Subtraction
-Multiplication
-Division
-Exponents (Hint: Look up Javascript's Math object)
-An operation of your choice!*/
