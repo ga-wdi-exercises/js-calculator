aed2a1ebfb6f0c30a9d45ccc41419c5d2fd06535
“SandyPok” <Pok.Sandy@yahoo.com>
2016-11-21 08:34:36 -0500

    All set with this assignment

--- calculator.js
+++ calculator.js
@@ -1,2 +1,21 @@
-var calculator = {
+let calculator = {
+  add: function(x,y){
+    return x + y;
+  },
+  sub: function(x,y){
+    return  x - y;
+  },
+  mul: function(x,y){
+    return  x * y;
+  },
+  div: function(x,y){
+    return  x/y;
+  },
+  exp: function(x,y){
+    return Math.pow(x, y);
+  }
+};
+function changeButton(i){
+  x = document.getElementById(i).innerHTML;
+    document.getElementById("choose").innerHTML = x;
 }
