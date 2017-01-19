81d2802a7e3c47dc5cc528db3d6099ab099c1e91
Samuel Clements <sdkclements@gmail.com>
2017-01-19 12:13:38 -0500

    set out basic html and JS

--- calculator.js
+++ calculator.js
@@ -1,2 +1,28 @@
-var calculator = {
+
+// var NumsInPlay = [];
+// var numA = NumsInPlay[0];
+// var numB = NumsInPlay[1];
+
+// var calculator = {
+
+// }
+
+function add(numA, numB){
+	return numA + numB;
+
 }
+
+function subtract(numA, numB){
+	return numA - numB;
+}
+
+function multiply(numA, numB){
+	return numA * numB;
+}
+function divide(numA, numB){
+	return numA / numB;
+}
+function remainder(numA, numB){
+	return numA % numB;
+}
+
