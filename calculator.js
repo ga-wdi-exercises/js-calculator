31a50bea14d0d1b5261e947d5418b3e5b6b3f6e9
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2017-01-22 16:13:24 -0500

    second commit

--- calculator.js
+++ calculator.js
@@ -1,18 +1,71 @@
 var calculator = {
   addition: function(a,b) {
-    return a+b;
-  }
+    if (calculator["currentVal"] === false) {
+      calculator["currentVal"] = a+b;
+      return a+b;
+    }
+    else {
+      calculator["currentVal"] = calculator["currentVal"]+b;
+      return calculator["currentVal"];
+    }
+  },
   subtraction: function(a,b) {
-    return a-b;
-  }
+    if (calculator["currentVal"] === false) {
+      calculator["currentVal"] = a-b;
+      return a-b;
+    }
+    else {
+      calculator["currentVal"] = calculator["currentVal"]-b;
+      return calculator["currentVal"];
+    }
+  },
   multiplication: function(a,b) {
-    return a*b;
-  }
+    if (calculator["currentVal"] === false) {
+      calculator["currentVal"] = a*b;
+      return a*b;
+    }
+    else {
+      calculator["currentVal"] = calculator["currentVal"]*b;
+      return calculator["currentVal"];
+    }
+  },
   division: function(a,b) {
-    return a/b;
-  }
+    if (calculator["currentVal"] === false) {
+      calculator["currentVal"] = a/b;
+      return a/b;
+    }
+    else {
+      calculator["currentVal"] = calculator["currentVal"]/b;
+      return calculator["currentVal"];
+    }
+
+  },
   exponentiation: function(a,b) {
-    return Math.pow(a, b);
+    if (calculator["currentVal"] === false) {
+      calculator["currentVal"] = Math.pow(a, b);
+      return Math.pow(a, b);
+    }
+    else {
+      calculator["currentVal"] = Math.pow(calculator["currentVal"],b);
+      return calculator["currentVal"];
+    }
+  },
+  modulo: function(a,b) {
+    if (calculator["currentVal"] === false) {
+      calculator["currentVal"] = a%b;
+      return a%b;
+    }
+    else {
+      calculator["currentVal"] = calculator["currentVal"]%b;
+      return calculator["currentVal"];
+    }
+    return a%b;
+  },
+  currentVal: false,
+  current: function() {
+    return calculator["currentVal"];
+  },
+  reset: function() {
+    calculator["currentVal"] = false;
   }
-  modulo: function(a,b)
 }
