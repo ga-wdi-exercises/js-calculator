397fb46db4da0a82dd7103c10779b8453f0a5df2
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2017-01-22 13:52:01 -0500

    first commit

--- calculator.js
+++ calculator.js
@@ -1,2 +1,18 @@
 var calculator = {
+  addition: function(a,b) {
+    return a+b;
+  }
+  subtraction: function(a,b) {
+    return a-b;
+  }
+  multiplication: function(a,b) {
+    return a*b;
+  }
+  division: function(a,b) {
+    return a/b;
+  }
+  exponentiation: function(a,b) {
+    return Math.pow(a, b);
+  }
+  modulo: function(a,b)
 }
