f2c04380f730534c5223042dc9285cd991fa73f4
Wes <wes.coffay@gmail.com>
2017-01-23 22:14:07 -0500

    experimented with memory tool

--- calculator.js
+++ calculator.js
@@ -5,28 +5,27 @@ var calculator = {
     return(x+y);
   },
   subtract: function(x,y) {
+    calculator.value=(calculator.value+x-y);
     return(x-y);
   },
   multiply: function(x,y) {
+    calculator.value=(calculator.value+(x*y));
     return(x*y);
   },
   divide: function(x,y) {
+    calculator.value=(calculator.value+(x/y));
     return(x/y);
   },
   exponent: function(x,y) {
+    calculator.value=(calculator.value+(Math.pow(x+y)));
     return(Math.pow(x,y));
   },
   max: function(x,y) {
+    calculator.value=(calculator.value+(Math.max(x,y)));
     return(Math.max(x,y));
   },
   clear: function() {
     calculator.value=0;
   }
 
-
-
-// // Give your calculator memory and allow it to persist the result of multiple operations.
-// // * Store this result in a `value` property.
-//
-// var 'value' = console.log(this);
 }
