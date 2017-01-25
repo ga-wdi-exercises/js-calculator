a81830ea979f4fc45fe7a3983ec10708e697ab3c
Natasha Clark <Natasharclark@gmail.com>
2017-01-25 18:22:35 -0500

    calc is mostly done, just need to work out the mem function

--- calculator.js
+++ calculator.js
@@ -1,2 +1,27 @@
-var calculator = {
+console.log('Hello World')
+
+var calc = {
+
+value = 0,
+
+ sum: function (x,y) {
+    calc.value=(calc.value+x+y);
+    return calc.value+ x+y; }
+  // },
+  // mult: function (x,y) {
+  //    console.log(calc.value+(x*y));
+  //    return x*y;
+  //  },
+  // div: function (x,y) {
+  //     console.log(calc.value+(x/y));
+  //     return x/y;
+  //   },
+  // sub: function (x,y) {
+  //       console.log(calc.value+(x-y));
+  //       return x-y;
+  //     },
+  // exp: function (x,y) {
+  //           console.log(calc.value+(x**y));
+  //           return x**y;
+  //         }
 }
