e395905fe72a7c297e37d2026cd05c572aaf1141
Samuel Clements <sdkclements@gmail.com>
2017-01-25 13:38:35 -0500

    mod html to equal js scripts

--- calculator.js
+++ calculator.js
@@ -1,47 +1,58 @@
+// 
+var calc ={
+	window: "",
+	add: (num1, num2) => num1 + num2,
+	subt: (num1, num2) => num1 - num2,
+	mult: (num1, num2) => num1 * num2,
+	divide: (num1, num2) => num1 / num2,
+	expon: (num, exp) => Math.pow(num ** exp),
+	modul: (num1, num2) => num1 % num2
+}
 
-var numsInPlay = [];
-var numA = numsInPlay[0];
-var numB = numsInPlay[1];
+console.log(calc.subt(4, 2))
+// var numsInPlay = [];
+// var numA = numsInPlay[0];
+// var numB = numsInPlay[1];
 
 //  var calculator = {
-	wait for button click 
-	enter number sequence, number one then number two
+	// wait for button click 
+	// enter number sequence, number one then number two
 
 
 // }
 
-function add(numA, numB){
-	return numA + numB;
+// function add(numA, numB){
+// 	return numA + numB;
 
-}
+// }
 
-function subtract(numA, numB){
-	return numA - numB;
-}
+// function subtract(numA, numB){
+// 	return numA - numB;
+// }
 
-function multiply(numA, numB){
-	return numA * numB;
-}
-function divide(numA, numB){
-	return numA / numB;
-}
-function remainder(numA, numB){
-	return numA % numB;
-}
+// function multiply(numA, numB){
+// 	return numA * numB;
+// }
+// function divide(numA, numB){
+// 	return numA / numB;
+// }
+// function remainder(numA, numB){
+// 	return numA % numB;
+// }
 
-function insertValue(num){
-	numsInPlay.push(num);
+// function insertValue(num){
+// 	numsInPlay.push(num);
 
-	addEventListener(click, isThisAnOperator(): EventListener, capture?: boolean)
-}
+// 	addEventListener(click, isThisAnOperator(): EventListener, capture?: boolean)
+// }
 
-function isThisAnOperator() {
+// function isThisAnOperator() {
 	//if an operator button is pressed so we chan then 
 	//change back into number entry mode
-}
-function placeInDoc(numop){
-	Document.getElementById('inputArea').innerhtml = numop
-}
+// }
+// function placeInDoc(numop){
+// 	Document.getElementById('inputArea').innerhtml = numop
+// }
 
 
 // function square(num){
@@ -51,14 +62,15 @@ function placeInDoc(numop){
 // function expon(num, exp){
 // 	return(num ** exp)
 // }
+
+// ---------------
+// const square = (num) => num ** 2;
+// const expon = (num, exp) => num ** exp;
+// console.log(expon(3, 2));
 // const addition = (num1, num2) => num1 + num2;
 // const subt = (num1, num2) => num1 - num2;
 // const mult = (num1, num2) => num1 * num2;
 // const divide = (num1, num2) => num1 / num2;
-// const modul = (num1, num2) => num1 % num2;
-//---------------
-// const square = (num) => num ** 2;
-// const expon = (num, exp) => num ** exp;
-// console.log(expon(3, 2));
+// const modul = 
 
 // console.log(square(10));
