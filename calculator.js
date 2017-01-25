c6876f6d1d453cdf4143b22262c69d85e84fccfa
Will Hawkins <willhawkins123@yahoo.com>
2017-01-25 14:25:36 -0500

    Finished Non-Bonus

--- calculator.js
+++ calculator.js
@@ -1,2 +1,21 @@
 var calculator = {
+  add:function(a, b) {
+    console.log(a + b);
+  },
+  subtract:function(a, b) {
+    console.log(a - b);
+  },
+  multiply:function(a, b) {
+    console.log(a*b);
+  },
+  divide:function(a, b) {
+    console.log(a / b);
+  },
+  powerOf:function(a, b) {
+    console.log((Math.pow(a, b)));
+  },
+  //divide the Input in Half and then Find the Square of that Number
+  squareHalf:function(a) {
+    console.log((a/2)*(a/2));
+  }
 }
