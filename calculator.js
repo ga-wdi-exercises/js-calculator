e50d76613f00a8551e09043b4370c9d0f3de0490
Collin OConnell <ococncol@gmail.com>
2017-01-24 19:17:03 -0500

    added basic objects to get calculator  working

--- calculator.js
+++ calculator.js
@@ -1,2 +1,14 @@
 var calculator = {
-}
+  add: function( a, b ) {
+    console.log( a + b );
+  },
+  subtract: function( a, b ) {
+    console.log( b - a );
+  },
+  multiply: function( a, b ) {
+    console.log(a * b);
+  },
+  divide: function ( a, b ) {
+    console.log( a / b );
+  }
+ }
