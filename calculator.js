7bdc2fd0b5aceb4c382608a63706c4b72eb363e2
Mitchell Carroll <jmitchellcarroll@gmail.com>
2017-01-25 18:02:37 -0500

    js caclulator homework

--- calculator.js
+++ calculator.js
@@ -1,2 +1,21 @@
-var calculator = {
+calculator = {
+  power: function( numOne, numTwo){
+  return (Math.pow(numOne, numTwo);
+  },
+  multiplication: function(numOne, numTwo){
+  (numOne * numTwo);
+  return numOne * numTwo;
+  },
+  subtraction: function(numOne, numTwo){
+  ( num1 - num2);
+  return numOne - numTwo;
+  },
+  addition: function(numOne, numTwo) {
+  (numOne+numTwo);
+  return numOne + numTwo;
+  },
+  square: function(numOne) {
+    (numOne * numOne);
+    return numOne * numOne;
+  }
 }
