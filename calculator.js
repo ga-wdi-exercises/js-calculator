5ef5fe0cf9e1d2a2481d696aa7fdc607004aa10b
Eva Lai <evalai.dc@gmail.com>
2017-01-25 14:01:20 -0500

    starting point

--- calculator.js
+++ calculator.js
@@ -1,2 +1,20 @@
 var calculator = {
+  function add(num1, num2) {
+
+  }
+  function subtract() {
+
+  }
+  function multiply() {
+
+  }
+  function divide() {
+
+  }
+  function exponents() {
+
+  }
+  function modulus () {
+
+  }
 }
