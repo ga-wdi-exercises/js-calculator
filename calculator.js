b261f3a9253ac3afd5b7038c054450ae88f7822a
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-01-25 14:21:45 -0500

    added functions to calculator object

--- calculator.js
+++ calculator.js
@@ -1,2 +1,28 @@
+
+
 var calculator = {
-}
+  add: function( num1, num2 ) {
+    console.log( num1 + num2 );
+  },
+
+  subtract: function(num1, num2) {
+    console.log( num1 - num2 );
+  },
+
+  multiply: function( num1, num2 ) {
+    console.log( num1 * num2 );
+   },
+
+  division: function( num1, num2) {
+    console.log(num1/num2);
+   },
+
+  exponent: function( num1, num2) {
+    console.log(Math.pow(num1, num2));
+  },
+
+  Modulus: function( num1, num2) {
+    console.log(num1 % num2);
+  }
+
+};
