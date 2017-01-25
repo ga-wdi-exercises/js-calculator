d0005612ea0de4573c31a81fc1819d6494649b36
Fiona <h.natapra@gmail.com>
2017-01-25 14:23:31 -0500

    initial commit

--- calculator.js
+++ calculator.js
@@ -1,2 +1,16 @@
+
 var calculator = {
+
+add: function (x,y) {
+	console.log(x+y);},
+
+substract: function (x,y) {
+	console.log(x-y);},
+
+multiply: function (x,y) {
+	console.log(x*y);},
+
+divide: function (x,y) {
+	console.log(x/y);}
+
 }
