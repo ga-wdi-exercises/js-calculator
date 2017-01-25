8591003a9cf83d7342a4d3f3f59e61927cb6795d
Bedig Galladian <bgalladian@gmail.com>
2017-01-25 14:08:57 -0500

    completed calculator

--- calculator.js
+++ calculator.js
@@ -1,2 +1,31 @@
 var calculator = {
+  add: function add(a,b){
+    console.log(a + b)
+    return a + b;
+  },
+
+  subtract: function subtract(a,b){
+    console.log(a - b)
+    return a - b;
+  },
+
+  multiply: function multiply(a,b){
+    console.log(a * b)
+    return a * b;
+  },
+
+  divide: function divide(a,b){
+    console.log(a / b)
+    return a / b;
+  },
+
+  exponent: function exponent(a,b){
+    console.log(Math.pow(a, b))
+  },
+
+  square_root: function square_root(a){
+    console.log(Math.sqrt(a))
+  }
+
+
 }
