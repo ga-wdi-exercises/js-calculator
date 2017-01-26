54427e482aecd94739ed4bda27f5cfa2c04d7497
Natasha Clark <Natasharclark@gmail.com>
2017-01-25 19:38:38 -0500

    Memory added

--- calculator.js
+++ calculator.js
@@ -1,24 +1,50 @@
+// console.log('Hello World')
+//
+// var calc = {
+//
+//  sum: function (x,y) {
+//      console.log(x+y);
+//      return x+ y;
+//   },
+//   mult: function (x,y) {
+//      console.log(x*y);
+//      return x*y;
+//    },
+//   div: function (x,y) {
+//       console.log(x/y);
+//       return x/y;
+//     },
+//   sub: function (x,y) {
+//         console.log(x-y);
+//         return x-y;
+//       },
+//   exp: function (x,y) {
+//           console.log(x**y);
+//           return x**y;}
+//         }
+
+
 console.log('Hello World')
+
+value = 0
 
 var calc = {
 
- sum: function (x,y) {
-     console.log(x+y);
-     return x+ y;
+ sum: function (x) {
+     return value =(value + x);
   },
-  mult: function (x,y) {
-     console.log(x*y);
-     return x*y;
+  mult: function (x) {
+     return value = (value *x);
    },
-  div: function (x,y) {
-      console.log(x/y);
-      return x/y;
+  div: function (x) {
+      return value=(value / x);
     },
-  sub: function (x,y) {
-        console.log(x-y);
-        return x-y;
+  sub: function (x) {
+        return value =(value - x);
       },
-  exp: function (x,y) {
-          console.log(x**y);
-          return x**y;}
-        }
+  exp: function (x) {
+          return value = (value ** x);
+        },
+  clear: function () {
+    return 0; }
+}
