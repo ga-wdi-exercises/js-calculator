025997bb71d2ab73e6f03c36c8ad831acdbbd584
Elena Santana <fanelenan7@gmail.com>
2017-01-26 12:19:31 -0500

    done

--- calculator.js
+++ calculator.js
@@ -1,2 +1,20 @@
 var calculator = {
+  add : function(num1, num2) {
+    console.log(num1 + num2);
+  },
+  sub : function(num1, num2) {
+    console.log(num1 - num2);
+  },
+  mult : function(num1, num2) {
+    console.log(num1 * num2);
+  },
+  div : function(num1, num2) {
+    console.log(num1 / num2);
+  },
+  exp : function(num1, num2) {
+    console.log(num1 ** num2);
+  },
+  perc : function (num1, num2) {
+    console.log(num1 / num2 * 100 + "%")
+  }
 }
