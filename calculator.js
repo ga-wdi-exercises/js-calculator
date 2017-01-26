3c42741d9139700043f4654860e9896680fbd430
Mike Nabil <maikel_nabil@yahoo.com>
2017-01-26 14:17:37 -0500

    laying down the basics

--- calculator.js
+++ calculator.js
@@ -1,2 +1,43 @@
 var calculator = {
+  firstElement : 0,
+  operation : 0 ,
+  secondElement : 0,
+  result : 0,
 }
+
+var add = function(n1, n2) {
+  console.log(n1 + n2);
+};
+
+var substract = function(n1, n2) {
+  console.log(n1 - n2);
+};
+
+var multiply = function(n1, n2) {
+  console.log(n1 * n2);
+};
+
+var devide = function (n1, n2) {
+  console.log( n1 / n2);
+};
+
+
+$("#plus").on("click", function () {
+    calculator['operation'] = add;
+    console.log(calculator);
+  });
+
+$("#minus").on("click", function () {
+    calculator[operation] = substract;
+    console.log(calculator);
+  });
+
+$("#mult").on("click", function () {
+    calculator[operation] = multiply;
+    console.log(calculator);
+  });
+
+$("#divid").on("click", function () {
+    calculator[operation] = devide;
+    console.log(calculator);
+  });
