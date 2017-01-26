68e44242d49a195f29001316c82a5755d882ac11
Natasha Clark <Natasharclark@gmail.com>
2017-01-25 19:23:46 -0500

    Calc with no memory

--- calculator.js
+++ calculator.js
@@ -2,26 +2,23 @@ console.log('Hello World')
 
 var calc = {
 
-value = 0,
-
  sum: function (x,y) {
-    calc.value=(calc.value+x+y);
-    return calc.value+ x+y; }
-  // },
-  // mult: function (x,y) {
-  //    console.log(calc.value+(x*y));
-  //    return x*y;
-  //  },
-  // div: function (x,y) {
-  //     console.log(calc.value+(x/y));
-  //     return x/y;
-  //   },
-  // sub: function (x,y) {
-  //       console.log(calc.value+(x-y));
-  //       return x-y;
-  //     },
-  // exp: function (x,y) {
-  //           console.log(calc.value+(x**y));
-  //           return x**y;
-  //         }
-}
+     console.log(x+y);
+     return x+ y;
+  },
+  mult: function (x,y) {
+     console.log(x*y);
+     return x*y;
+   },
+  div: function (x,y) {
+      console.log(x/y);
+      return x/y;
+    },
+  sub: function (x,y) {
+        console.log(x-y);
+        return x-y;
+      },
+  exp: function (x,y) {
+          console.log(x**y);
+          return x**y;}
+        }
