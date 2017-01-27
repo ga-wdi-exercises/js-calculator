704ace025e874537543b8d8ebe591edd8e50b522
Erica Norby <ericanorby@gmail.com>
2017-01-26 22:27:53 -0500

    created calculator object with memory

--- calculator.js
+++ calculator.js
@@ -1,2 +1,31 @@
+var value = 0;
+
 var calculator = {
+  add: function(b) {
+    value = value + b;
+    return value;
+  },
+  subtract: function(b) {
+    value = value - b;
+    return value;
+  },
+  multiply: function(b) {
+    value = value * b;
+    return value;
+  },
+  divide: function(b) {
+    value = value / b;
+    return value;
+  },
+  powerOf: function(b) {
+    value = value ** b;
+    return value;
+  },
+  square: function() {
+    value = value * value;
+    return value;
+  },
+  clear: function() {
+    value = 0;
+  }
 }
