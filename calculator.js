f50b00c889b49bd5c1fcdbf632949654e63bf11b
Laura Goldstein <laura.koss.goldstein@gmail.com>
2017-01-26 19:02:12 -0500

    added calculator.js

--- calculator.js
+++ calculator.js
@@ -1,2 +1,20 @@
 var calculator = {
-}
+  addition: function(num1, num2){
+    console.log(num1 + num2);
+  },
+  subtraction: function(num1,num2){
+    console.log(num1 - num2);
+  },
+  multiplication: function(num1, num2){
+    console.log(num1 * num2);
+  },
+  division: function(num1,num2){
+    console.log(num1 / num2);
+  },
+  exponents: function(num1,num2){
+    console.log(Math.pow(num1,num2));
+  },
+  remainder: function(num1,num2){
+    console.log(num1 % num2);
+  }
+};
