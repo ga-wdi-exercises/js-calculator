1826023f98ddbb02deea2caeae0f763b8a8724a3
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-01-26 19:17:09 -0500

    completed js calculator homework

--- calculator.js
+++ calculator.js
@@ -1,2 +1,21 @@
 var calculator = {
+  addition: function(a, b) {
+    console.log(a+b);
+  },
+
+  subtraction: function(a, b) {
+    console.log(a-b);
+  },
+
+  multiplication: function(a, b) {
+    console.log(a*b);
+  },
+
+  division: function(a, b) {
+    console.log(a/b);
+  },
+
+  square: function(a) {
+    console.log(a*a);
+  },
 }
