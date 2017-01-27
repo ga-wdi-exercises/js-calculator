55e73cc37099897748191c74bea6da6e3da3ceb8
Mike Nabil <maikel_nabil@yahoo.com>
2017-01-26 21:59:52 -0500

    made the calculator object in the calculator.js file

--- calculator.js
+++ calculator.js
@@ -1,43 +1,20 @@
 var calculator = {
-  firstElement : 0,
-  operation : 0 ,
-  secondElement : 0,
-  result : 0,
+  add : function(n1, n2) {
+    console.log(n1 + n2);
+  },
+  substract : function(n1, n2) {
+    console.log(n1 - n2);
+  },
+  multiply : function(n1, n2) {
+    console.log(n1 * n2);
+  },
+  devide : function (n1, n2) {
+    console.log( n1 / n2);
+  },
+  exponent : function (n1, n2) {
+    console.log(n1 ** n2);
+  },
+  celsius : function (n1) {
+    console.log( (n1 - 32) * (5/9) );
+  },
 }
-
-var add = function(n1, n2) {
-  console.log(n1 + n2);
-};
-
-var substract = function(n1, n2) {
-  console.log(n1 - n2);
-};
-
-var multiply = function(n1, n2) {
-  console.log(n1 * n2);
-};
-
-var devide = function (n1, n2) {
-  console.log( n1 / n2);
-};
-
-
-$("#plus").on("click", function () {
-    calculator['operation'] = add;
-    console.log(calculator);
-  });
-
-$("#minus").on("click", function () {
-    calculator[operation] = substract;
-    console.log(calculator);
-  });
-
-$("#mult").on("click", function () {
-    calculator[operation] = multiply;
-    console.log(calculator);
-  });
-
-$("#divid").on("click", function () {
-    calculator[operation] = devide;
-    console.log(calculator);
-  });
