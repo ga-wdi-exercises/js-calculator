633aabc39a3bd2660e7b8a291cb8fa4b88116c19
Kay Ehni <kay.ehnigmail.com>
2017-01-27 06:12:25 -0500

    calculator HW

--- calculator.js
+++ calculator.js
@@ -1,2 +1,26 @@
 var calculator = {
+add : function (x,y) {
+  var sum = x + y;
+  console.log (sum);
+  },
+subtract : function (x,y) {
+var diff = x - y;
+console.log (diff);
+  },
+multiply : function  (x,y) {
+var product = x * y;
+console.log (product);
+  },
+divide : function (x,y) {
+var quotient = x/y;
+console.log (quotient);
+  },
+exponent : function (x,y) {
+  var power = Math.pow(x,y);
+  console.log (power);
+  },
+remainder : function (x,y) {
+  var remain = x%y;
+  console.log (remain);
+}
 }
