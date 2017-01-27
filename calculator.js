dcf6cf40569fc1ca1eede8c1c11cfe7eef6ef48e
Lisa Anne Burr <burr_lisa@bah.com>
2017-01-26 21:36:13 -0500

    added pwer and sq root Math objects

--- calculator.js
+++ calculator.js
@@ -1,8 +1,11 @@
 var x = 2;
-var y = 3;
+var y = 4;
+
 var add = x + y;
 var sub = x - y;
 var mult = x * y;
 var dvd = x / y;
+var pow = Math.pow(x, y);
+var sqrt = Math.sqrt(y);
 
-console.log(add, sub, mult, dvd);
+console.log(add, sub, mult, dvd, pow, sqrt);
