a69462cf401effa7c6ad93d880c091525909813b
Jonathan Wright <jonwright.va@gmail.com>
2017-03-23 21:52:15 -0400

    added functions, still unstable, working on stability

--- calculator.js
+++ calculator.js
@@ -1,2 +1,63 @@
 var calculator = {
+	add: function() {
+		var returnVal;
+		if (calculator.value === null) {
+			returnVal = 0;
+		} else {
+			returnVal = calculator.value;
+		}
+		for (var i = 0; i < arguments.length; i++) {
+			returnVal += arguments[i];
+		}
+		calculator.value = returnVal;
+		console.log(calculator.value);
+	},
+
+	subtract: function() {
+		var returnVal;
+		if (calculator.value === null) {
+			returnval = arguments[0];
+		} else {
+			returnVal = calculator.value;
+		}
+		for (var i = 0; i < arguments.length; i++) {
+			returnVal -= arguments[i];
+		}
+		calculator.value = returnVal;
+		console.log(calculator.value);
+	},
+
+	mutiply: function(a, b) {
+    var returnVal;
+		if (calculator.value === null) {
+			returnval = arguments[0];
+		} else {
+			returnVal = calculator.value;
+		}
+		for (var i = 0; i < arguments.length; i++) {
+			returnVal *= arguments[i];
+		}
+		calculator.value = returnVal;
+		console.log(calculator.value);
+	},
+  
+	divide: function(a, b) {
+		calculator.value = a / b;
+		calculator.returnVal();
+	},
+	exponent: function(a, b) {
+		calculator.value = Math.pow(a, b);
+		calculator.returnVal();
+	},
+	square_root: function(a) {
+		calculator.value = Math.sqrt(a);
+		calculator.returnVal();
+	},
+	value: null,
+	clear: function() {
+		calculator.value = null;
+	},
+	returnVal: function() {
+		console.log(calculator.value);
+	}
 }
