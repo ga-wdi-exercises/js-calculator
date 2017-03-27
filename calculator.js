def027d8a6e0a542241052dfbc1a094605c26652
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-03-27 18:49:22 -0400

    added multiplication, division, and exponential functions

--- calculator.js
+++ calculator.js
@@ -2,9 +2,22 @@ var calculator = {
   add: function(num1,num2) {
     return num1 + num2;
     console.log(num1 + num2);
-  }
+  },
+  //subtract
   subtract: function(num1,num2) {
     return num1 - num2;
     console.log(num1 + num2);
-  }
+  },
+  //multiply
+  multiply: function(num1,num2) {
+     return num1 * num2;
+  },
+  //divide
+  divide: function(num1,num2) {
+     return num1 / num2;
+  },
+  //exponent
+  expo: function(num1,pow) {
+     return Math.pow(num1,pow)
+   }
 }
