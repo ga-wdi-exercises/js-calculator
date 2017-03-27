2504a6cab2987f1d1f95a101c6c5601e8f89f92a
Erin <erinedawson@gmail.com>
2017-03-27 14:26:36 -0400

    add basic calculator functions

--- calculator.js
+++ calculator.js
@@ -1,2 +1,7 @@
 var calculator = {
-}
+  add: function(num1, num2){return num1 + num2},
+  subtract: function(num1, num2){return num1 - num2},
+  multiply: function(num1, num2){return num1 * num2},
+  divide: function(num1, num2){return num1/num2},
+  remainder: function(num1, num2){return num1%num2}
+};
