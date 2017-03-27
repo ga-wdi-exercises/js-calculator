eb63d9088f6c12531f754410d7d738d4a4b152a7
Erin <erinedawson@gmail.com>
2017-03-27 18:53:16 -0400

    add exponent function

--- calculator.js
+++ calculator.js
@@ -3,5 +3,6 @@ var calculator = {
   subtract: function(num1, num2){return num1 - num2},
   multiply: function(num1, num2){return num1 * num2},
   divide: function(num1, num2){return num1/num2},
-  remainder: function(num1, num2){return num1%num2}
+  remainder: function(num1, num2){return num1%num2},
+  exponent: function(num1, num2){return Math.pow(num1, num2)}
 };
