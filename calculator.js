48778851923825b9cbef3794bbc396b2d9427ea6
Whitney <whitney.purdum@gmail.com>
2017-03-27 13:00:56 -0400

    git init

--- calculator.js
+++ calculator.js
@@ -1,2 +1,24 @@
 var calculator = {
+	multiply: function(num1, num2){
+		console.log(num1 * num2)
+	},
+
+	subtract: function(num1, num2) {
+		console.log(num1 - num2);
+	},
+
+	add: function(num1, num2) {
+		console.log(num1 + num2);
+	},
+
+	divide: function(num1, num2) {
+		console.log(num1 / num2);
+	},
+
+	remainder: function(num1, num2) {
+		console.log(num1 % num2);
+	}
+
 }
+
+
