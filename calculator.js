f9ca492e6fd98617971b8110627c3a7585a38529
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-03-27 18:41:57 -0400

    added an object key for subtraction

--- calculator.js
+++ calculator.js
@@ -1,6 +1,10 @@
 var calculator = {
   add: function(num1,num2) {
-    return num1 + num2
-    console.log(num1 + num2)
+    return num1 + num2;
+    console.log(num1 + num2);
+  }
+  subtract: function(num1,num2) {
+    return num1 - num2;
+    console.log(num1 + num2);
   }
 }
