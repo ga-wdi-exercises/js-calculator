9d0dd12189a37c169da83f77302f290c0eacc2ce
Caroline Chen <carolinecaroline@gmail.com>
2017-03-27 22:03:55 -0400

    initial commit

--- calculator.js
+++ calculator.js
@@ -1,2 +1,18 @@
 var calculator = {
+	add: function(x, y) {
+		console.log(x + y);
+	},
+	subtract: function(x, y) {
+		console.log(x - y);
+	},
+	multiply: function(x, y) {
+		console.log(x * y);
+	},
+	divide: function(x, y) {
+		console.log(x / y);
+	},
+	exponent: function(x, y) {
+		console.log(Math.pow(x, y));
+	}
 }
+
