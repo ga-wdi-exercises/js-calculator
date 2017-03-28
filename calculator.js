7b0e61d33fb2935dc4fd9f3a98c69e2ab7a5ce73
Ick Hyun Kwon <kwonih1020@gmail.com>
2017-03-28 12:38:51 -0400

    edited errors

--- calculator.js
+++ calculator.js
@@ -1,24 +1,37 @@
 var calculator = { }
 
+var x = add (5,4);
 function add (num1, num2) {
-   console.log(num1 + num2);
+  //  console.log(num1 + num2);
    return num1 + num2;
- };
-    add (5,4);
+ }
+x;
 
+var x = sub(6,4);
 function sub (num1, num2) {
-   console.log(num1 - num2);
+  //  console.log(num1 - num2);
    return num1 - num2;
- };
-    sub (6,4);
+ }
+x;
 
+var x = multiply (5,4);
 function multiply (num1, num2) {
-   console.log(num1 * num2);
+  //  console.log(num1 * num2);
    return num1 * num2;
- };
-    multiply (5,4);
+ }
+x;
 
+var x = division (6,3);
 function division (num1, num2) {
   return num1 / num2;
 }
-  division (6,3);
+x;
+
+function exponent (num1, num2) {
+    var sum = 1;
+    for (num2; num2 > 0; num2--){
+    sum = sum * num1 ;
+  }
+    return sum;
+  }
+  exponent (6,2);
