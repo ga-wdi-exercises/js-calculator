c86b6b7e80a44e707c13f7493be65e28b18b76d0
Marcus Linn Miller Jr <marcusmiller220@gmail.com>
2017-03-28 11:30:19 -0400

    calculator

--- calculator.js
+++ calculator.js
@@ -1,2 +1,42 @@
 var calculator = {
 }
+
+calculator.add = function(num1,num2) {
+  return num1 + num2;
+};
+
+calculator.add (2, 4)
+
+calculator.substract = function(num1,num2) {
+  return num1 - num2;
+};
+
+calculator.substract (4,3)
+
+calculator.multiply = function(num1,num2){
+  return num1 * num2;
+}
+
+calculator.multiply (8,4)
+
+calculator.divide = function(num1, num2) {
+  return num1 / num2;
+}
+
+calculator.divide (9,3)
+
+calculator.exponent = function(num1,num2) {
+  var sum = 1
+  for(num2; num2 > 0; num2--){
+  sum = sum * num1
+  }
+  return sum;
+}
+
+ calculator.exponent (6,2)
+
+ calculator.decrement = function(num1) {
+   return num1--;
+ }
+
+calculator.decrement (9)
