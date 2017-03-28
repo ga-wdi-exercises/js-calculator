4a3be470113c50037088af2af593c6477f5dd695
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-03-28 14:26:12 -0400

    add a for loop that stores the value of the addition function

--- calculator.js
+++ calculator.js
@@ -1,12 +1,16 @@
 var calculator = {
-  add: function(num1,num2) {
-    return num1 + num2;
-    console.log(num1 + num2);
+  //add
+  add: function() {
+    //loop through and add the length of arguments
+    for (var i = 0; i < arguments.length; i++) {
+      calculator.value += arguments[i]
+    }
+    console.log(calculator.value);
   },
+
   //subtract
   subtract: function(num1,num2) {
     return num1 - num2;
-    console.log(num1 + num2);
   },
   //multiply
   multiply: function(num1,num2) {
@@ -23,9 +27,10 @@ var calculator = {
   //remainder
   modulo: function(num1,num2) {
     return num1%num2
-  }
-}
-
-if (calculator.value == ''){
-
+  },
+  clear: function() {
+    calculator.value = null;
+  },
+  //store value
+  value: null,
 }
