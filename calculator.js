46ec6f600ccc62d8720351aeed4e7901b20de3d8
Andres Arango <andresarangov17@gmail.com>
2017-03-28 19:15:19 -0400

    calculator without bonuses

--- calculator.js
+++ calculator.js
@@ -1,2 +1,37 @@
 var calculator = {
+  add: function() {
+    total = 0;
+    for (var i = 0; i < arguments.length; i++) {
+      total += arguments[i];
+    }
+    return total;
+  },
+  subtract: function() {
+    total = arguments[0];
+    for (var i = 1; i < arguments.length; i++) {
+      total -= arguments[i];
+    }
+    return total;
+  },
+  multiply: function() {
+    total = 1;
+    for (var i = 0; i < arguments.length; i++) {
+      total *= arguments[i];
+    }
+    return total;
+  },
+  divide: function() {
+    total = arguments[0];
+    for (var i = 1; i < arguments.length; i++) {
+      total /= arguments[i];
+    }
+    return total;
+  },
+  exponential: function(base, exponent) {
+    total = 1;
+    for (var i = 0; i < exponent; i++) {
+      total *= base;
+    }
+    return total;
+  }
 }
