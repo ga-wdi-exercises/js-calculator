5ee8af720eda30f9bd4677b80e9b1386c2a4b06a
Ick Hyun Kwon <kwonih1020@gmail.com>
2017-03-28 17:49:15 -0400

    recode from start

--- calculator.js
+++ calculator.js
@@ -1,49 +1,64 @@
-var calculator = { }
+var calculator = {
+  value: 0,
+  add: function(x, y) {
+    if(y) {
+      var sum = x + y
+    } else {
+      var sum = this.value + x ;
+    }
+    this.value = sum
+    return sum
+  },
+  subtract: function(x, y) {
+    if(y) {
+      var sum = x - y
+    } else {
+      var sum = this.value - x ;
+    }
+    this.value = sum
+    return sum
+  },
+  multiply: function(x, y) {
+    if(y) {
+      var sum = x * y
+    } else {
+      var sum = this.value * x ;
+    }
+    this.value = sum
+    return sum
+  },
+  division: function(x, y) {
+    if(y) {
+      var sum = x / y
+    } else {
+      var sum = this.value / x ;
+    }
+    this.value = sum
+    return sum
+  },
 
-var x = add (5,4);
-function add (num1, num2) {
-   return num1 + num2;
- }
-x;
-
-var x = sub(6,4);
-function sub (num1, num2) {
-   return num1 - num2;
- }
-x;
-
-var x = multiply (5,4);
-function multiply (num1, num2) {
-   return num1 * num2;
- }
-x;
-
-var x = division (6,3);
-function division (num1, num2) {
-  return num1 / num2;
 }
-x;
-
-function exponent (num1, num2) {
-    var sum = 1;
-    for (num2; num2 > 0; num2--){
-    sum = sum * num1 ;
-  }
-    return sum;
-  }
-exponent (6,2);
-
-
-var i = 0;
-var c = i++;
-      c = ++i;
-
-var i = 10;
-var c = i++;
-      c = ++i;
-      c = --i;
-      c = ++i + i++;
-      c = ++i - i++;
-      c = ++i * i++;
-      c = ++i / i++;
-alert(c);
+//
+// function exponent (num1, num2) {
+//     var sum = 1;
+//     for (num2; num2 > 0; num2--){
+//     sum = sum * num1 ;
+//   }
+//     return sum;
+//   }
+// exponent (6,2);
+//
+//
+// var i = 0;
+// var c = i++;
+//       c = ++i;
+//
+// var i = 10;
+// var c = i++;
+//       c = ++i;
+//       c = --i;
+//       c = ++i + i++;
+//       c = ++i - i++;
+//       c = ++i * i++;
+//       c = ++i / i++;
+// alert(c);
