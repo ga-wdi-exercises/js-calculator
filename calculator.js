73b5d242413be2c75e2af6e891bc02ab30acfede
Adam Hoff <swimmerdude79@comcast.net>
2017-03-28 09:39:26 -0400

    created calculator with memory

--- calculator.js
+++ calculator.js
@@ -1,2 +1,51 @@
+value = 0;
 var calculator = {
+  add: function(num1, num2){
+    value = num1 + num2;
+    return value;
+  },
+  add: function(num1){
+    value = num1 + value;
+    return value;
+  },
+  subtract: function(num1, num2){
+    value = num1 - num2;
+    return value;
+  },
+  subtract: function(num1){
+    value = value - num1;
+    return value;
+  },
+  multiply: function(num1, num2){
+    value = num1*num2;
+    return value;
+  },
+  multiply: function(num1){
+    value = value * num1;
+    return value;
+  },
+  divide: function(num1, num2){
+    value = num1/num2;
+    return value;
+  },
+  divide: function(num1){
+    value = value/num1;
+    return value;
+  },
+  powerOf: function(num1, num2){
+    value = Math.pow(num1, num2);
+    return value;
+  },
+  powerOf: function(num1){
+    value = Math.pow(value, num1);
+    return value;
+  },
+  nthRoot: function(num1, num2){
+    value = Math.pow(num1, 1/num2);
+    return value;
+  },
+  nthRoot: function(num1){
+    value = Math.pow(value, 1/num1);
+    return value;
+  }
 }
