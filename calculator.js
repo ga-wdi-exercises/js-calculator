b5ff3392b00393354cf7406698a00122b557c24c
Ick Hyun Kwon <kwonih1020@gmail.com>
2017-03-27 23:04:31 -0400

    simple operator

--- calculator.js
+++ calculator.js
@@ -1,2 +1,22 @@
 var calculator = {
 }
+
+function add (num1, num2) {
+   console.log(num1 + num2);
+   return num1 + num2;
+ }
+
+ add (5,4);
+
+ function sub (num1, num2) {
+   console.log(num1 - num2);
+   return num1 - num2;
+ }
+
+ sub (6,4);
+
+ function multiply (num1, num2) {
+   console.log(num1 * num2);
+   return num1 * num2;}
+
+multiply (5,4);
