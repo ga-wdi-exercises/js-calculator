f4d92e32ed6c16e783bd473eaf8956f36e717aad
Adam Hoff <swimmerdude79@comcast.net>
2017-03-29 08:09:57 -0400

    fixed alt functions

--- calculator.js
+++ calculator.js
@@ -1,10 +1,10 @@
-value = 0;
+var value = 0;
 var calculator = {
   add: function(num1, num2){
     value = num1 + num2;
     return value;
   },
-  add: function(num1){
+  add1: function(num1){
     value = num1 + value;
     return value;
   },
@@ -12,7 +12,7 @@ var calculator = {
     value = num1 - num2;
     return value;
   },
-  subtract: function(num1){
+  subtract1: function(num1){
     value = value - num1;
     return value;
   },
@@ -20,7 +20,7 @@ var calculator = {
     value = num1*num2;
     return value;
   },
-  multiply: function(num1){
+  multiply1: function(num1){
     value = value * num1;
     return value;
   },
@@ -28,7 +28,7 @@ var calculator = {
     value = num1/num2;
     return value;
   },
-  divide: function(num1){
+  divide1: function(num1){
     value = value/num1;
     return value;
   },
@@ -36,7 +36,7 @@ var calculator = {
     value = Math.pow(num1, num2);
     return value;
   },
-  powerOf: function(num1){
+  powerOf1: function(num1){
     value = Math.pow(value, num1);
     return value;
   },
@@ -44,7 +44,7 @@ var calculator = {
     value = Math.pow(num1, 1/num2);
     return value;
   },
-  nthRoot: function(num1){
+  nthRoot1: function(num1){
     value = Math.pow(value, 1/num1);
     return value;
   }
