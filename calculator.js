5823c9d8cd4ffb25998d1fb72770a01d0bf5835f
amlak befekadu <amlakgirma@yahoo.com>
2017-03-29 20:21:09 -0400

    calculator

--- calculator.js
+++ calculator.js
@@ -1,2 +1,21 @@
+
+var value;
 var calculator = {
-}
+  add: function(num1, num2){
+    value = num1 + num2;
+    return value;
+  },
+
+  sub: function(num1, num2){
+
+
+    return num1 - num2;
+  },
+  multi: function(num1, num2){
+    return num1 * num2;
+  },
+  div: function(num1, num2){
+    return num1 / num2;
+  },
+  
+};
