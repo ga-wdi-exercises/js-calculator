a945f1ee09322e21f61c2dc9d0d9a52a26994187
Caroline Chen <carolinecaroline@gmail.com>
2017-03-29 23:21:33 -0400

    add functionality

--- calculator.js
+++ calculator.js
@@ -1,18 +1,50 @@
 var calculator = {
-	add: function(x, y) {
-		console.log(x + y);
+
+	add: function() {
+		var result = 0;
+		for (var i = 0; i < arguments.length; i++) {
+			result = result + arguments[i];
+		}
+		console.log(result);
 	},
-	subtract: function(x, y) {
-		console.log(x - y);
+
+
+	subtract: function() {
+		var result = arguments[0];
+		for (var i = 1; i < arguments.length; i++){
+			result = result - arguments[i];
+		}
+		console.log(result);
 	},
-	multiply: function(x, y) {
-		console.log(x * y);
+
+
+	multiply: function() {
+		var result = 1;
+		for (var i = 0; i < arguments.length; i++) {
+			result = result * arguments[i];
+		}
+		console.log(result);
 	},
-	divide: function(x, y) {
-		console.log(x / y);
+
+
+	divide: function() {
+		var result = arguments[0];
+		for (var i = 1; i < arguments.length; i++){
+			result = result / arguments[i];
+		}
+		console.log(result);
 	},
-	exponent: function(x, y) {
-		console.log(Math.pow(x, y));
+
+
+	exponent: function() {
+		var result = arguments[0];
+		for (var i = 1; i < arguments.length; i++){
+			result = Math.pow(result, arguments[i])
+		}
+		console.log(result);
+	},
+
+	negate: function(x) {
+		console.log(x * -1);
 	}
 }
-
