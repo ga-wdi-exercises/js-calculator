c225d2431e4b445492e7ab8c48d643ddcb43543b
tedlayea <tedlayea@gMAIL.com>
2017-05-11 01:32:10 -0400

    modified

--- calculator.js
+++ calculator.js
@@ -1,2 +1,27 @@
-var calculator = {
+var calculator= {
+add: function (num1,num2){
+      console.log(num1+num2);
+      return "The sum of these two number is : "+num1 + num2;
+    },
+sub: function (num1, num2){
+      console.log(num1-num2);
+      return num1 - num2;
+  },
+multiply:  function (num1,num2){
+      console.log(num1*num2 );
+      return num1 * num2;
+    },
+divide:  function (num1,num2){
+      console.log(num1/num2);
+      return num1 / num2;
+    },
+expo:  function (num1,num2){
+      console.log(num1**num2 );
+      return num1 ** num2;
+    },
+formula: function (num1,num2){
+      console.log(10*num1+num2);
+      return 10*num1 + num2 ;
+    }
+
 }
