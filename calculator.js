944dbdddbbf05c312a321733ef7dd2d9af0857ab
Robert Lowe <RobLowe1992@Yahoo.com>
2017-05-10 23:07:46 -0400

    Finished Calculator

--- calculator.js
+++ calculator.js
@@ -1,2 +1,37 @@
 var calculator = {
+    add: function(num1, num2) {
+        var value = num1 + num2;
+        this.value.push(value);
+        console.log(value);
+    },
+    subtract: function(num1, num2) {
+        var value = num1 - num2;
+        this.value.push(value);
+        console.log(value);
+    },
+    multiply: function(num1, num2) {
+        var value = num1 * num2;
+        this.value.push(value);
+        console.log(value);
+    },
+    divide: function(num1, num2) {
+        var value = num1 / num2;
+        this.value.push(value);
+        console.log(value);
+    },
+    exponent: function(base, power) {
+        var value = base ** power;
+        this.value.push(value);
+        console.log(value);
+    },
+    remainder: function(num1, num2) {
+        var value = num1 % num2;
+        this.value.push(value);
+        console.log(value);
+    },
+    value: [],
+    clear: function() {
+        this.value = [];
+    }
+
 }
