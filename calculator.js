963f2f93506a48f0c549dd6e8b59fd3a4884b650
Robert Lowe <RobLowe1992@Yahoo.com>
2017-05-11 00:17:24 -0400

    Finished Calculator

--- calculator.js
+++ calculator.js
@@ -1,37 +1,59 @@
 var calculator = {
+    value: 0,
     add: function(num1, num2) {
-        var value = num1 + num2;
-        this.value.push(value);
-        console.log(value);
+        if (num2 === undefined) {
+            num2 = this.value;
+        }
+        let returnValue = num1 + num2;
+        this.value = returnValue;
+        return returnValue;
+
     },
     subtract: function(num1, num2) {
-        var value = num1 - num2;
-        this.value.push(value);
-        console.log(value);
+        if (num2 === undefined) {
+            num2 = this.value;
+        }
+        let returnValue = num2 - num1;
+        this.value = returnValue;
+        return returnValue;
     },
     multiply: function(num1, num2) {
-        var value = num1 * num2;
-        this.value.push(value);
-        console.log(value);
+        if (num2 === undefined) {
+            num2 = this.value;
+        }
+        let returnValue = num2 * num1;
+        this.value = returnValue;
+        return returnValue;
     },
     divide: function(num1, num2) {
-        var value = num1 / num2;
-        this.value.push(value);
-        console.log(value);
+        if (num2 === undefined) {
+            num2 = this.value;
+        }
+        let returnValue = num2 / num1;
+        this.value = returnValue;
+        return returnValue;
     },
     exponent: function(base, power) {
-        var value = base ** power;
-        this.value.push(value);
-        console.log(value);
+        if (power === undefined) {
+            power = base;
+            base = this.value
+        }
+            let returnValue = base ** power;
+            this.value = returnValue;
+            return returnValue;
     },
     remainder: function(num1, num2) {
-        var value = num1 % num2;
-        this.value.push(value);
-        console.log(value);
+        if (num2 === undefined) {
+            num2 = this.value;
+        }
+        let returnValue = num2 % num1;
+        this.value = returnValue;
+        return returnValue;
     },
-    value: [],
     clear: function() {
-        this.value = [];
+        this.value = 0;
+        let returnValue = this.value;
+        return returnValue;
     }
 
 }
