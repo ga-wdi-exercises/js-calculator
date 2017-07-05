684890ecda396d70c6c467c36ad498f6946e5846
MATT FILBERT <mcfilbert@email.wm.edu>
2017-07-05 14:41:58 -0400

    made bonus calc

--- calculator.js
+++ calculator.js
@@ -1,2 +1,37 @@
+
+var y
+var value = 0
 var calculator = {
+  add: function (y) {
+    value = value + y
+    return value
+  },
+  subtract: function (y) {
+    value = value - y
+    return value
+  },
+  multiply: function (y) {
+    value = value * y
+    return value
+  },
+  divide: function (y) {
+    value = value / y
+    return value
+  },
+  remainder: function (y) {
+    value = value % y
+    return value
+  },
+  exponent: function (y) {
+    value = Math.pow(value, y)
+    return value
+  },
+  master: function (y) {
+    value = y
+    return value
+  },
+  clear: function () {
+    value = 0
+    return value
+  }
 }
