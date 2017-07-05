b81cac8577882585cf9e654f0274cda72ab4963b
MATT FILBERT <mcfilbert@email.wm.edu>
2017-07-05 17:13:10 -0400

    add DOM manip

--- calculator.js
+++ calculator.js
@@ -4,34 +4,46 @@ var value = 0
 var calculator = {
   add: function (y) {
     value = value + y
+    changeText()
     return value
   },
   subtract: function (y) {
     value = value - y
+    changeText()
     return value
   },
   multiply: function (y) {
     value = value * y
+    changeText()
     return value
   },
   divide: function (y) {
     value = value / y
+    changeText()
     return value
   },
   remainder: function (y) {
     value = value % y
+    changeText()
     return value
   },
   exponent: function (y) {
     value = Math.pow(value, y)
+    changeText()
     return value
   },
   master: function (y) {
     value = y
+    changeText()
     return value
   },
   clear: function () {
     value = 0
+    changeText()
     return value
   }
+}
+
+function changeText () {
+  document.getElementById('calc').innerHTML = `${value}`
 }
