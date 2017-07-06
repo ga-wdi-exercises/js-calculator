88bbd514e594d29436f30c1f2baa717512cf5689
Kendall <adkins.kendall90@gmail.com>
2017-07-06 18:52:04 -0400

    add logic

--- calculator.js
+++ calculator.js
@@ -3,21 +3,25 @@ console.log('My JS Calculator')
 // object - calculator
 var calculator = {
   value: 0,
-
-  add: function (a, b) {
-    if (b === undefined) {
-      b = this.value
-    }
-    this.value = a + b
-    return a + b
+// checks undefined in b -- sets value... resumes operation
+  add: function (a, b = 0) {
+    // if (b === undefined) {
+    //   b = this.value
+    // }
+    this.value += a + b
+    return this.value
   },
-
+// similar to add, checks for undefined in b... sets value... then checks
+// undefined in a && b and subtracts b - a
   subtract: function (a, b) {
     if (b === undefined) {
       b = this.value
       this.value = b - a
+    } else if (a !== undefined && b !== undefined) {
+      console.log(`subtracting a & b from current value of: ${this.value}`)
+      this.value = this.value - a - b
     }
-    return b - a
+    return this.value
   },
 
   multiply: function (a, b) {
@@ -56,8 +60,4 @@ var calculator = {
     this.value = b % a
     return this.value
   }
-
-  // master: funtion () {
-  //
-  // }
 }
