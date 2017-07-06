352c8acbd222e01d75b942abb06f8e09d12cf948
Jacob Greenwald <jeg4t@virginia.edu>
2017-07-06 18:12:19 -0500

    initial commit

--- calculator.js
+++ calculator.js
@@ -1,2 +1,21 @@
+var value = y
 var calculator = {
+  add: function (a, b) {
+  return value += (a + b)
+},
+  subtract: function (a, b) {
+    return value + (a - b)
+  },
+  multiply: function (a, b) {
+    return value + (a * b)
+  },
+  divide: function (a, b) {
+    return value + (a / b)
+  },
+  exponent: function (a, b) {
+    return value + (Math.pow(a, b))
+  },
+  remainder: function (a, b) {
+    return value + (a % b)
+  }
 }
