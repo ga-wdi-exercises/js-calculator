984e81615cf3b3ca52c8c232128af21b9bf11d4b
Kendall <adkins.kendall90@gmail.com>
2017-07-05 21:30:37 -0400

    add logic

--- calculator.js
+++ calculator.js
@@ -1,2 +1,63 @@
+console.log('My JS Calculator')
+
+// object - calculator
 var calculator = {
+  value: 0,
+
+  add: function (a, b) {
+    if (b === undefined) {
+      b = this.value
+    }
+    this.value = a + b
+    return a + b
+  },
+
+  subtract: function (a, b) {
+    if (b === undefined) {
+      b = this.value
+      this.value = b - a
+    }
+    return b - a
+  },
+
+  multiply: function (a, b) {
+    if (b === undefined) {
+      b = this.value
+    }
+    this.value = a * b
+    return a * b
+  },
+
+  divide: function (a, b) {
+    if (b === undefined) {
+      b = this.value
+    }
+    this.value = b / a
+    return this.value
+  },
+
+  exponent: function (a, b) {
+    if (b === undefined) {
+      b = this.value
+    }
+    this.value = Math.pow(b, a)
+    return this.value
+  },
+
+  clear: function () {
+    this.value = 0
+    return this.value
+  },
+
+  modulo: function (a, b) {
+    if (b === undefined) {
+      b = this.value
+    }
+    this.value = b % a
+    return this.value
+  }
+
+  // master: funtion () {
+  //
+  // }
 }
