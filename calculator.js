534a1a82840230843a573d835b965869f56274a4
Jacob Greenwald <jeg4t@virginia.edu>
2017-07-06 18:45:35 -0500

    add calc, stored val and reset val

--- calculator.js
+++ calculator.js
@@ -1,21 +1,55 @@
-var value = y
 var calculator = {
+  value: 0,
   add: function (a, b) {
-  return value += (a + b)
-},
+    if (b === undefined) {
+      return this.value += a
+    } else {
+      return this.value = a + b
+    }
+    return this.value
+  },
   subtract: function (a, b) {
-    return value + (a - b)
+    if (b === undefined) {
+      return this.value -= a
+    } else {
+      return this.value = a - b
+    }
+    return this.value
   },
   multiply: function (a, b) {
-    return value + (a * b)
+    if (b === undefined) {
+      return this.value * a
+    } else {
+      return this.value = a * b
+    }
+    return this.value
   },
   divide: function (a, b) {
-    return value + (a / b)
+    if (b === undefined) {
+      return this.value / a
+    } else {
+      return this.value = a / b
+    }
+    return this.value
   },
   exponent: function (a, b) {
-    return value + (Math.pow(a, b))
+    if (b === undefined) {
+      return this.value ** a
+    } else {
+      return this.value = a ** b
+    }
+    return this.value
   },
   remainder: function (a, b) {
-    return value + (a % b)
+    if (b === undefined) {
+      return this.value % a
+    } else {
+      return this.value = a % b
+    }
+    return this.value
+  },
+  resetValue: function () {
+    this.value = 0
+    console.log('memory cleared')
   }
 }
