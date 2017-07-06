bd2666acc6facae47142adacdeeedf0137785c4f
MATT FILBERT <mcfilbert@email.wm.edu>
2017-07-06 10:14:16 -0400

    cleaned up code

--- calculator.js
+++ calculator.js
@@ -3,62 +3,56 @@ var y
 var value = 0
 var calculator = {
   add: function (y, x) {
-    if (x === undefined){
-    value = value + y
-  }
-  else {
-    value = y + x
-  }
+    if (x === undefined) {
+      value = value + y
+    } else {
+      value = y + x
+    }
     changeText()
     return value
   },
   subtract: function (y, x) {
-    if (x === undefined){
-    value = value - y
-  }
-  else {
-    value = y - x
-  }
+    if (x === undefined) {
+      value = value - y
+    } else {
+      value = y - x
+    }
     changeText()
     return value
   },
   multiply: function (y, x) {
-    if (x === undefined){
+    if (x === undefined) {
       value = value * y
-  }
-    else {
+    } else {
       value = y * x
     }
     changeText()
     return value
   },
   divide: function (y, x) {
-    if (x === undefined){
-    value = value / y
-  }
-  else {
-    value = y / x
-  }
+    if (x === undefined) {
+      value = value / y
+    } else {
+      value = y / x
+    }
     changeText()
     return value
   },
   remainder: function (y, x) {
-    if (x === undefined){
-    value = value % y
-  }
-  else {
-    value = y % x
-  }
+    if (x === undefined) {
+      value = value % y
+    } else {
+      value = y % x
+    }
     changeText()
     return value
   },
   exponent: function (y, x) {
-    if (x === undefined){
-    value = Math.pow(value, y)
-  }
-  else {
-    value = Math.pow(y, x)
-  }
+    if (x === undefined) {
+      value = Math.pow(value, y)
+    } else {
+      value = Math.pow(y, x)
+    }
     changeText()
     return value
   },
