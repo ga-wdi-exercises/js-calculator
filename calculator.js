9270d06220e4198d9bd3a00ab984945b6d4cb0bb
Sanjay Persaud <sanjaywallah@gmail.com>
2017-07-08 17:23:47 -0400

    created calculator object

--- calculator.js
+++ calculator.js
@@ -1,2 +1,23 @@
-var calculator = {
+ var calculator = {
+   //these anonymous functions can also be written
+   //in the form var = (parameter/s) => function
+   //no "return" statement necessary.
+  add: function (a, b) {
+   return a + b
+  },
+  subtract: function (a, b) {
+   return a - b
+  },
+  multiply: function (a, b) {
+    return a * b
+  },
+  divide: function (a, b) {
+    return a / b
+  },
+  exponent: function (a, b) {
+    return Math.pow(a, b)
+  },
+  remainder: function (a, b) {
+    return a % b
+  }
 }
