74658bb15f599e875cad588cf218afda9baee664
Rami Taha <ramisn3@gmail.com>
2017-07-09 21:31:44 -0400

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -1,2 +1,28 @@
 var calculator = {
+  value: 0,
+  add: function (a) {
+    this.value = this.value + a
+    return this.value
+  },
+  sub: function (a) {
+    this.value = this.value - a
+    return this.value
+  },
+  multi: function (a, b) {
+    this.value = a * b
+    return this.value
+  },
+  division: function (a, b) {
+    this.value = a / b
+    return this.value
+  },
+  expo: function (a, b) {
+    this.value = Math.pow(a, b)
+    return this.value
+  },
+  sqrt: function (a) {
+    this.value = Math.sqrt(a)
+    return this.value
+  }
+
 }
