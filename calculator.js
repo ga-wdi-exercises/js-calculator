98e197ec42e97e7713a5f5ac0f711f4bbd46b842
Patrick Reese <reespa01@gmail.com>
2017-07-27 16:03:39 -0400

    message

--- calculator.js
+++ calculator.js
@@ -1,106 +1,22 @@
-function Calculator () {
-  this.answers = []
-}
+class Calculator {
 
-calculator.prototype.add = function (v1, v2) {
-  var current
-
-  if(v2 === undefined && current.length > 0) {
-    current = (v1 + answers[this.answers.length -1])
-    this.answers.push(current)
-    return current
-  }
-  else if (v2 === undefined) {
-    return null
-  }
-  else {
-    current = (v1 + v2)
-    this.answers.push(current)
-    return current
+   add(a,b){
+    return a + b
   }
 
-}
+  subtract(a,b){
+    return a - b
+  }
 
-calculator.prototype.subtract = function (v1, v2) {
-  var current
+  divide(a,b){
+    return a / b
+  }
 
-  if(v2 === undefined && current.length > 0) {
-    current  = (v1 - answers[this.answers.length -1])
-    this.answers.push(current)
-    return current
+  multiply(a,b){
+    return a * b
   }
-  else if (v2 === undefined) {
-    return null
-  }
-  else {
-    current = (v1 - v2)
-    this.answers.push(current)
-    return current
-  }
-}
 
-calculator.prototype.multiply = function (v1, v2) {
-  var current
-
-  if(v2 === undefined && current.length > 0) {
-    current = (v1 * answers[this.answers.length -1])
-    this.answers.push(current)
-    return current
-  }
-  else if (v2 === undefined) {
-    return null
-  }
-  else {
-    current = (v1 * v2)
-    this.answers.push(current)
-    return current
-  }
-}
-
-calculator.prototype.divide = function (v1, v2) {
-  var current
-
-  if(v2 === undefined && current.length > 0) {
-    current = (v1 / answers[this.answers.length -1])
-    this.answers.push(current)
-    return current
-  }
-  else if (v2 === undefined) {
-    return null
-  }
-  else {
-    current = (v1 / v2)
-    this.answers.push(current)
-    return current
-  }
-}
-
-calculator.prototype.exponent = function (v1, v2) {
-  var current
-
-  if(v2 === undefined && current.length > 0) {
-    current = (Math.pow(v1, answers[this.answers.length -1]))
-    this.answers.push(current)
-    return current
-  }
-  else if (v2 === undefined) {
-    return null
-  }
-  else {
-    current = Math.pow(v1, v2)
-    this.answers.push(current)
-    return current
-}
-
-calculator.prototype.toZero = function (v1, v2) {
-  if(v2 === undefined && !this.answers) {
-    this.answers.push(0)
-  }
-  else if (v2 === undefined) {
-    return null
-  }
-  else {
-    this.answers.push(0)
-    return (0)
+  random(a){
+    return a * (Math.floor(Math.random() * 20))
   }
 }
