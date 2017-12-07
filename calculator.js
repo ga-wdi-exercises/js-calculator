9543924168ac86a0b41166928bd8a98e270a0762
Hammad Malik <hmd.node@gmail.com>
2017-12-07 07:56:52 -0500

    Implement calculator.multiply() method

--- calculator.js
+++ calculator.js
@@ -4,5 +4,13 @@ var calculator = {
   },
   subtract: (num1, num2) => {
     return num1 - num2
+  },
+  multiply: (num1, num2) => {
+    return num1 * num2
   }
+  // divide
+  // exponent
+  // remainder
 }
+
+console.log(calculator)
