efdfbb82326c382d9c962e8b7ed0c511a3a9c6d3
Hammad Malik <hmd.node@gmail.com>
2017-12-07 21:20:36 -0500

    Add .memory property and .clearMemory() method to calculator object

--- calculator.js
+++ calculator.js
@@ -1,23 +1,47 @@
 var calculator = {
-  add: (num1, num2) => {
-    return num1 + num2
+  memory: 0,
+  clearMemory: function () {
+    this.memory = 0
   },
-  subtract: (num1, num2) => {
-    return num1 - num2
+  add: function (addend1, addend2) {
+    let sum = addend1 + addend2
+    this.memory = sum
+    return sum
   },
-  multiply: (num1, num2) => {
-    return num1 * num2
+  subtract: function (minuend, subtrahend) {
+    let difference = minuend - subtrahend
+    this.memory = difference
+    return difference
   },
-  divide: (num1, num2) => {
-    return num1 / num2
+  multiply: function (factor1, factor2) {
+    let product = factor1 * factor2
+    this.memory = product
+    return product
   },
-  exponentiate: (num1, num2) => {
-    return num1 ** num2
+  divide: function (dividend, divisor) {
+    let quotient = dividend / divisor
+    this.memory = quotient
+    return quotient
   },
-  remainder: (num1, num2) => {
-    return num1 % num2
+  exponentiate: function (base, exponent) {
+    let result = base ** exponent
+    this.memory = result
+    return result
+  },
+  remainder: function (dividend, divisor) {
+    let result = dividend % divisor
+    this.memory = result
+    return result
   }
 }
 
 // to get rid of Standard JS error that calculator is assigned a value but never used
 console.log(calculator)
+
+/* sources:
+names of part of arithmetic equations:
+https://www.montereyinstitute.org/courses/DevelopmentalMath/TEXTGROUP-1-8_RESOURCE/U01_L5_T1_text_final.html
+
+names of parts of exponential equations:
+https://math.stackexchange.com/a/799800
+*/
